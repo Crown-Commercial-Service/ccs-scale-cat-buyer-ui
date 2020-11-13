6a4b678712c8ad7c6170ee7b974ad40fc5cb122c
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-11-13 12:04:46 +0000

    SINF-186 - WIP

--- scripts/php_build.sh
+++ scripts/php_build.sh
@@ -2,4 +2,4 @@
 
 echo "PHP build placeholder (without this it will use default script and attempt to run currently non-existent phpunit tests)"
 
-echo $CLOUDFOUNDRY_USERNAME
+echo "CLOUDFOUNDRY_USERNAME=$CLOUDFOUNDRY_USERNAME"
