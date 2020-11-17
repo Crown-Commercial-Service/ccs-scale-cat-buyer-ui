e707186a2220794c7f854b6861492c4282d2ba38
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-11-17 15:35:12 +0000

    SINF-186 - tidy home page

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -25,7 +25,7 @@ class APIController
         $content = $response->getContent();
 
         return new Response(
-            '<html><body><h2>Endpoint: '.$_ENV["PRIVATE_APP_URL"] .'/agreement-summaries</h2><p>' . $content . '</p></body></html>'
+            '<html><body><h2>CaT Gov.UK PaaS Demo</h2><img src="lolcatz.jpg" /><h3>Request:</h3><p>'.$_ENV["PRIVATE_APP_URL"] .'/agreement-summaries</p><h3>Response:</h3><p>' . $content . '</p></body></html>'
         );
     }
 }
