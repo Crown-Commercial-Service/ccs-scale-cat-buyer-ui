3ea75f9520beb63f23a2de4cb53303115ac09fb2
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-11-17 14:53:27 +0000

    Feature/sinf 186 create skeleton (#2)
    SINF-186 - skeleton UI

---
+++ src/Controller/APIController.php
@@ -0,0 +1,31 @@
+<?php
+// src/Controller/APIController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class APIController
+{
+
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    public function getData(): Response
+    {
+        $response = $this->client->request(
+            'GET',
+            $_ENV["PRIVATE_APP_URL"]. "/agreement-summaries"
+        );
+
+        $content = $response->getContent();
+
+        return new Response(
+            '<html><body><h2>Endpoint: '.$_ENV["PRIVATE_APP_URL"] .'/agreement-summaries</h2><p>' . $content . '</p></body></html>'
+        );
+    }
+}
