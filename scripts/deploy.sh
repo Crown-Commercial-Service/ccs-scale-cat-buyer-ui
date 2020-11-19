3b72136d182da897919c3350f1dbaabcf457067a
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-11-19 11:16:10 +0000

    SINF-211 - WIP

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,8 +8,9 @@ set -meo pipefail
 
 cf --version
 cf api https://api.london.cloud.service.gov.uk
-cf login -u $CLOUDFOUNDRY_USERNAME -p $CLOUDFOUNDRY_PASSWORD -o ccs-scale-cat -s $CF_SPACE \
-cf push --var CF_ROUTE_PREFIX-prefix=$CF_ROUTE_PREFIX \
-		--var CF_MEMORY=$CF_MEMORY \
-		--var ROLLBAR_ACCESS_TOKEN=$ROLLBAR_ACCESS_TOKEN \
+cf login -u $CLOUDFOUNDRY_USERNAME -p $CLOUDFOUNDRY_PASSWORD -o ccs-scale-cat -s $CF_SPACE
+cf push --var CF_ROUTE_PREFIX=$CF_ROUTE_PREFIX \
+        --var CF_MEMORY=$CF_MEMORY \
+        --var AGREEMENTS_SERVICE_URL=$AGREEMENTS_SERVICE_URL \
+        --var ROLLBAR_ACCESS_TOKEN=$ROLLBAR_ACCESS_TOKEN \
 		--var ROLLBAR_ENVIRONMENT=$ROLLBAR_ENVIRONMENT
