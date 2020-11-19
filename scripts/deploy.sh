9bca7b3e4de9887db9b8b5024f918430430aab41
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-11-19 11:11:20 +0000

    SINF-211 - fixed deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Login to GPaaS London Cloud Foundry ccs-scale-cat org with space determined by environment variable CF_SPACE.
-# Push local app based on manifest, setting vars `route-prefix` from env var `CF_ROUTE_PREFIX` and `memory` from `CF_MEMORY` 
+# Push local app based on manifest, setting vars `CF_ROUTE_PREFIX` from env var `CF_ROUTE_PREFIX` and `CF_MEMORY` from `CF_MEMORY` 
 #
 
 set -meo pipefail
@@ -9,7 +9,7 @@ set -meo pipefail
 cf --version
 cf api https://api.london.cloud.service.gov.uk
 cf login -u $CLOUDFOUNDRY_USERNAME -p $CLOUDFOUNDRY_PASSWORD -o ccs-scale-cat -s $CF_SPACE
-cf push --var CF_ROUTE_PREFIX-prefix=$CF_ROUTE_PREFIX 
-		--var CF_MEMORY=$CF_MEMORY 
-		--var ROLLBAR_ACCESS_TOKEN=$ROLLBAR_ACCESS_TOKEN 
+cf push --var CF_ROUTE_PREFIX-prefix=$CF_ROUTE_PREFIX \
+		--var CF_MEMORY=$CF_MEMORY \
+		--var ROLLBAR_ACCESS_TOKEN=$ROLLBAR_ACCESS_TOKEN \
 		--var ROLLBAR_ENVIRONMENT=$ROLLBAR_ENVIRONMENT
