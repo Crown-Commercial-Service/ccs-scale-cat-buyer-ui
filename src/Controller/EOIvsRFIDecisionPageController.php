2af7197596f562a9ec03771b34ff9ddc03345e58
sorin.durtea <sorin.durtea@192-168-0-108.rdsnet.ro>
2020-12-14 18:07:52 +0200

    add redirect to the next page

--- src/Controller/EOIvsRFIDecisionPageController.php
+++ src/Controller/EOIvsRFIDecisionPageController.php
@@ -3,11 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class EOIvsRFIDecisionPageController extends AbstractController
 {
 
-    public function eoiRfiDecision(){
+    public function eoiRfiDecision(Request $request){
+
+        if($request->getMethod() == "POST"){
+
+            $postData = $request->request->all();
+//            dd($postData);
+            if($postData['eoi-rfi'] == 'EOI'){
+                return $this->redirect('/eoi-overview');
+            }
+            return $this->redirect('/rfi-overview');
+        }
+
         return $this->render('pages/eoi_vs_rfi_decision.html.twig', [
             "pageTitle" => "EOI vs RFI Decision"
         ]);
