b935cf70a225944c4997739d8ef461eda7ba22f6
sorin.durtea <sorin.durtea@192-168-0-108.rdsnet.ro>
2020-12-15 14:45:24 +0200

    naming a procurement page

--- src/Controller/ProcurementNamePageController.php
+++ src/Controller/ProcurementNamePageController.php
@@ -2,14 +2,23 @@
 
 namespace App\Controller;
 
+use App\Models\ComercialAgreement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProcurementNamePageController extends AbstractController
 {
 
     public function procurementName(){
+
+        $comercialAgreement = new ComercialAgreement();
+        $comercialCANumber = $comercialAgreement->getCANumber();
+        $comercialLOTNumber = $comercialAgreement->getlotNumber();
+
         return $this->render('pages/procurement_name_page.html.twig', [
-            "pageTitle" => "Name Your Procurement Project"
+            "pageTitle" => "Name Your Procurement Project",
+            "pageSubTitle" => "You can use your organisation's internal project naming criteria to create a suitable name for your procurement project",
+            "comercialCANumber" => $comercialCANumber,
+            "comercialLOTNumber" => $comercialLOTNumber
         ]);
     }
 }
