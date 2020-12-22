3ecb04336d4b4270d5f58c005932a228b4e77832
adrianmos1980 <adrian.mos@cognizant.com>
2020-12-22 10:32:28 +0200

    add supliers page

---
+++ src/Models/SupliersList.php
@@ -0,0 +1,78 @@
+<?php 
+
+namespace App\Models;
+
+class SupliersList{
+
+
+    private $regionSelectedNr = 8;
+    private $serviceTypeSelectedNr = 2;
+    private $suplierDeselectedNr = 2;
+
+    private $documentSupliersPath = "../files/Supplier Shortlist Ex - With Deselection.xlsx";
+    private $documentSupliersName = "Supplier Shortlist Ex - With Deselection.xlsx";
+
+    private $supliers =[
+        0 => [
+            'name' => '1ST BIOTECH LIMITED',
+            'small_madium_ent' => 1
+        ],
+
+        1 => [
+            'name' => 'Express Linen Services Limited'
+        ],
+        2 => [
+            'name' => 'Northern Care Alliance NHS Group'
+        ],
+        3 => [
+            'name' => 'Synergy LMS',
+            'small_madium_ent' => 1
+        ],
+        4 => [
+            'name' => 'CENTRAL LAUNDRY LIMITED',
+            'small_madium_ent' => 1
+        ],
+        5 => [
+            'name' => 'Communication Services'
+        ],
+        6 => [
+            'name' => 'Contact Centre Services'
+        ],
+        7 => [
+            'name' => 'Costs Lawyer Services'
+        ],
+        8 => [
+            'name' => 'Spike95 Limited'
+        ],
+        9 => [
+            'name' => 'Swiss Post Solutions Ltd'
+        ]
+    ];
+
+    public function getList(){
+            return $this->supliers;
+    }
+
+    public function getRegionSelectedNr(){
+        return $this->regionSelectedNr;
+    }
+
+    public function getServiceTypeSelectedNr(){
+        return $this->serviceTypeSelectedNr;
+    }
+
+    public function getSuplierDeselectedNr(){
+        return $this->suplierDeselectedNr;
+    }
+
+    public function getDocumentSupliersPath(){
+        return $this->documentSupliersPath;
+    }
+
+    public function getDocumentSupliersName(){
+        return $this->documentSupliersName;
+    }
+
+}
+
+?>
