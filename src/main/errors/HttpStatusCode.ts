99b1d68e5548629d3bbf90c34802fe0f8c73716e
Abdul Daim <89971948+daimdev@users.noreply.github.com>
2021-09-24 16:20:01 +0100

    modified and simplified components, added Http Status Code BP.

---
+++ src/main/errors/HttpStatusCode.ts
@@ -0,0 +1,9 @@
+
+export enum HttpStatusCode {
+    FORBIDDEN_ACCESS = 403,
+    OK = 200,
+    BAD_REQUEST = 400,
+    NOT_FOUND = 404,
+    INTERNAL_SERVER = 500,
+   }
+   
--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,11 +1,8 @@
 import { Application } from 'express';
+import * as Path from '../global/Path'
+import * as CONTROLLER   from '../controllers/Home'
 
 export default function(app: Application): void {
-
-  app.get('/', (req, res) => {
-
-
-    res.render('home');
-  });
-
+  
+  app.get(Path.LandingPath.LANDING , CONTROLLER.LandingHandler);
 }
