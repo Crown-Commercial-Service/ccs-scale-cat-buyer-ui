2ed56a0d2c844329eac489c68d7e9855a5f829e1
Alvarez Laviada <2057586@cognizant.com>
2021-09-28 15:01:21 +0100

    Added conditional to populate TaskList.njk with dynamic data from rfiTaskList.json. Also, added variables for src/test/routes/rfiTaskList.ts

--- src/test/routes/rfiTaskList.ts
+++ src/test/routes/rfiTaskList.ts
@@ -1,15 +1,15 @@
 import { expect } from 'chai';
 import request from 'supertest';
+import {RFI_PATHS} from '../../main/features/RFI/path/rfiPath'
+import {RFI_TASKLIST} from '../../main/features/RFI/controller/rfiTaskList'
 
 import { app } from '../../main/app';
 
 // TODO: replace this sample test with proper route tests for your application
 describe('RFI TASK LIST', () => {
   describe('on GET', () => {
-    test('should return sample rfi Task List ', async () => {
-      await request(app)
-        .get('/rfi/rfi-tasklist')
-        .expect((res) => expect(res.status).to.equal(200));
-    });
+    test('should return sample rfi Task List ', async () => await request(app)
+      .get(RFI_PATHS.RFI_TASKLIST, RFI_TASKLIST as any)
+      .expect((res) => expect(res.status).to.equal(200)));
   });
 });
