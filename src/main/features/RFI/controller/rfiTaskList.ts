82ec2d58962653f6ce259f47cccaab1c67b6bba5
Abdul Daim <89971948+daimdev@users.noreply.github.com>
2021-09-28 11:04:23 +0100

    Added RFI CMS json details, added controllers and updated view to generate content dynamically.

--- src/main/features/RFI/controller/rfiTaskList.ts
+++ src/main/features/RFI/controller/rfiTaskList.ts
@@ -1,6 +1,9 @@
 import * as express from 'express'
+import * as cmsData from '../../../resources/content/RFI/rfiTaskList.json'
 
 // RFI TaskList
 export const RFI_TASKLIST = (req : express.Request, res : express.Response)=> {
-   res.render('features/RFI/Tasklist'); 
+
+   var windowAppendData = {data : cmsData}
+   res.render('features/RFI/Tasklist', windowAppendData); 
 }
