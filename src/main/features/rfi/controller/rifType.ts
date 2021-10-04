b55f214973bf600ac05e20f4f42e00184bde088c
pradhap87 <89925857+prathap-cognizant@users.noreply.github.com>
2021-10-04 16:10:08 +0100

    removed duplicates

--- src/main/features/rfi/controller/rifType.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express'
-import * as cmsData from '../../../resources/content/RFI/rfiType.json'
-
-// RFI TaskList
-export const RFI_TYPE = (req : express.Request, res : express.Response)=> {
-   var windowAppendData = {data : cmsData}
-   res.render('type', windowAppendData); 
-}
