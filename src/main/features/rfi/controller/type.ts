357aa10e7951aff8d6aed0bf53351f269ed8f2f8
Abdul Daim <89971948+daimdev@users.noreply.github.com>
2021-10-11 10:36:43 +0100

    refactored the type json file and fixed the njk

--- src/main/features/rfi/controller/type.ts
+++ src/main/features/rfi/controller/type.ts
@@ -1,5 +1,6 @@
 import * as express from 'express'
 import * as cmsData from '../../../resources/content/RFI/rfiType.json'
+import { ObjectModifiers } from '../util/operations/objectremoveEmptyString';
 
 // RFI TaskList
 export const GET_TYPE = (req : express.Request, res : express.Response)=> {
@@ -18,8 +19,21 @@ export const GET_TYPE = (req : express.Request, res : express.Response)=> {
 //POST 'rfi/type'
 export const POST_TYPE = (req : express.Request, res : express.Response)=> {
    let {agreement_id} = req.query;
+   let filtered_body_content_removed_rfi_key =    ObjectModifiers._deleteKeyofEntryinObject(req.body, 'choose_rfi_type');
 
-   // do something else 
-   let redirect_address = `/rfi/online-task-list?agreement_id=${agreement_id}`;
-   res.redirect(redirect_address);
+   let {ccs_rfi_type} = filtered_body_content_removed_rfi_key;
+
+   switch(ccs_rfi_type){
+      case 'all_online':
+         let redirect_address = `/rfi/online-task-list?agreement_id=${agreement_id}`;
+          res.redirect(redirect_address);
+      break;
+
+      case 'all_offline':
+         // implement the logic here. 
+      break;
+
+      default: res.redirect('/404');
+   }
+  
 }
