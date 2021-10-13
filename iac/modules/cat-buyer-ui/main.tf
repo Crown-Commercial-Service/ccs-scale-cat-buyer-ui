1626774b65713f251945ab93298a433941b8e378
Alvarez Laviada <2057586@cognizant.com>
2021-10-13 11:53:38 +0100

    fixed environ variables error in file iac/modules/cat-buyer-ui/main.tf

--- iac/modules/cat-buyer-ui/main.tf
+++ iac/modules/cat-buyer-ui/main.tf
@@ -34,6 +34,18 @@ data "cloudfoundry_user_provided_service" "ip_router" {
   space = data.cloudfoundry_space.space.id
 }
 
+data "aws_ssm_parameter" "env_auth_server_client_id" {
+  name = "/cat/${var.environment}/auth-server-client-id"
+}
+
+data "aws_ssm_parameter" "env_auth_server_client_secret" {
+  name = "/cat/${var.environment}/auth-server-client-secret"
+}
+
+data "aws_ssm_parameter" "env_auth_server_base_url" {
+  name = "/cat/${var.environment}/auth-server-base-url"
+}
+
 resource "cloudfoundry_app" "cat_buyer_ui" {
   annotations = {}
   buildpack   = var.buildpack
@@ -42,6 +54,9 @@ resource "cloudfoundry_app" "cat_buyer_ui" {
   environment = {
     TENDERS_SERVICE_API_URL : "http://${var.environment}-ccs-scale-cat-service.apps.internal:8080"
     AGREEMENTS_SERVICE_API_URL : "https://${var.environment}-ccs-scale-shared-agreements-service.london.cloudapps.digital"
+    AUTH_SERVER_CLIENT_ID : data.aws_ssm_parameter.env_auth_server_client_id.value
+    AUTH_SERVER_CLIENT_SECRET : data.aws_ssm_parameter.env_auth_server_client_secret.value
+    AUTH_SERVER_BASE_URL : data.aws_ssm_parameter.env_auth_server_base_url.value
   }
   health_check_timeout = var.healthcheck_timeout
   health_check_type    = "port"
@@ -124,36 +139,6 @@ data "aws_ssm_parameter" "env_auth_server_client_secret" {
 
 data "aws_ssm_parameter" "env_auth_server_base_url" {
   name = "/cat/${var.environment}/auth-server-base-url"
-}
-
-resource "cloudfoundry_app" "cat_buyer_ui" {
-  annotations = {}
-  buildpack   = var.buildpack
-  disk_quota  = var.disk_quota
-  enable_ssh  = true
-  environment = {
-    TENDERS_SERVICE_API_URL : "http://${var.environment}-ccs-scale-cat-service.apps.internal:8080"
-    AGREEMENTS_SERVICE_API_URL : "https://${var.environment}-ccs-scale-shared-agreements-service.london.cloudapps.digital"
-    AUTH_SERVER_CLIENT_ID : data.aws_ssm_parameter.env_auth_server_client_id.value
-    AUTH_SERVER_CLIENT_SECRET : data.aws_ssm_parameter.env_auth_server_client_secret.value
-    AUTH_SERVER_BASE_URL : data.aws_ssm_parameter.env_auth_server_base_url.value
-  }
-  health_check_timeout = var.healthcheck_timeout
-  health_check_type    = "port"
-  instances            = var.instances
-  labels               = {}
-  memory               = var.memory
-  name                 = "${var.environment}-ccs-scale-cat-buyer-ui"
-  path                 = data.archive_file.cat_buyer_ui.output_path
-  source_code_hash     = data.archive_file.cat_buyer_ui.output_base64sha256
-  ports                = [8080]
-  space                = data.cloudfoundry_space.space.id
-  stopped              = false
-  timeout              = 600
-
-  service_binding {
-    service_instance = data.cloudfoundry_user_provided_service.logit.id
-  }
 }
 
 resource "cloudfoundry_route" "cat_buyer_ui" {
