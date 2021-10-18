fa6784f976f1338ccb3a7904a1a10a6031afb34f
pradhap87 <89925857+prathap-cognizant@users.noreply.github.com>
2021-10-18 14:27:33 +0100

    refactoring for SIT deployment

--- src/main/features/rfi/controller/onlinetasklist.ts
+++ src/main/features/rfi/controller/onlinetasklist.ts
@@ -1,5 +1,5 @@
 import * as express from 'express'
-import { DynamicFrameworkInstance } from '../util/fetch/dyanmicframeworkInstance';
+// import { DynamicFrameworkInstance } from '../util/fetch/dyanmicframeworkInstance';
 import fileData from '../../../resources/content/RFI/rfionlineTaskList.json'
 
 // RFI TaskList
@@ -8,9 +8,9 @@ export const GET_ONLINE_TASKLIST = async (req : express.Request, res : express.R
    var {agreement_id} = req.query;
    
    try {
-      let fetch_dynamic_api = await DynamicFrameworkInstance.Instance.get('');
-      let fetch_dynamic_api_data = fetch_dynamic_api.data; 
-      let select_default_data_from_fetch_dynamic_api = fetch_dynamic_api_data.default;
+      // let fetch_dynamic_api = await DynamicFrameworkInstance.Instance.get('');
+      // let fetch_dynamic_api_data = fetch_dynamic_api.data; 
+      let select_default_data_from_fetch_dynamic_api = fileData;
       var display_fetch_data = {data : select_default_data_from_fetch_dynamic_api, agreement_id: agreement_id, file_data: fileData}
       res.render('onlinetasklist', display_fetch_data); 
    } catch (error) {
