5b2513eca9de3dc8a94094f7a7a19d2e10b06489
Abdul Daim <89971948+daimdev@users.noreply.github.com>
2021-10-21 17:25:08 +0100

    refactored signout functionality

--- src/main/features/auth/controller/oauthLogout.ts
+++ src/main/features/auth/controller/oauthLogout.ts
@@ -11,7 +11,8 @@ import config from 'config';
 export const OAUTH_LOGOUT = async (req : express.Request, res : express.Response)=> {
     res.clearCookie('state'); 
     res.clearCookie('SESSION_ID');
-    const paramsUrl = `redirect-uri=${config.get('authenticationService.logout_callback')}&client-id=${process.env.AUTH_SERVER_CLIENT_ID}`;
+   
+    const paramsUrl = `client-id=${process.env.AUTH_SERVER_CLIENT_ID}&redirect-uri=${process.env.CAT_URL+config.get('authenticationService.logout_callback')}`;
     const logoutUrl = `${process.env.AUTH_SERVER_BASE_URL}${config.get('authenticationService.logout')}?${paramsUrl}`;
     res.redirect(logoutUrl);
 }
