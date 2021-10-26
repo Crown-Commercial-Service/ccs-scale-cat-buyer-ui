718088ceb68574a59cdbdd2156bcde03d7e33bda
ulysses <ulysses.alvarez@cognizant.com>
2021-10-26 08:54:58 +0100

    cleaning.

--- src/main/common/middlewares/agreementservice/chooseagreement.ts
+++ src/main/common/middlewares/agreementservice/chooseagreement.ts
@@ -18,11 +18,9 @@ export class ChooseAgreementMiddleware {
         let retrieveAgreementPromise = AgreementAPI.Instance.get(BaseURL)
         retrieveAgreementPromise.then( (data)=> {
             let containedData = data?.data;
-            //console.log(':::::::::::::::::::::::::::::::::::::::::: ULYSSES 555 containedData :::::::::::::::::::::::::::::::::: ', containedData)
             res.locals.project_agreement = containedData;
             res.locals.project_agreement_htmlText = htmlToText.convertor(containedData.description);
             res.locals.sortedItems = sortObject.sort_by(containedData.lots, "number", true);
-            console.log(':::::::::::::::::::::::::::::::::::::::::: ULYSSES 66666 res.locals.sortedItems :::::::::::::::::::::::::::::::::: ', res.locals.sortedItems)
             next(); 
 
         }).catch(
