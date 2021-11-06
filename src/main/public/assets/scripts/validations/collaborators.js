3e9729e35b06d6c0b10a2e181e87da6cdfbad125
Abdul Daim <89971948+daimdev@users.noreply.github.com>
2021-11-06 13:24:02 +0000

    fixed bugs for hot reloading, authentication state check middleware

--- src/main/public/assets/scripts/validations/collaborators.js
+++ src/main/public/assets/scripts/validations/collaborators.js
@@ -1,101 +1,111 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  if (document.getElementById("ccs_add_collab") !== null) {
+  if (document.getElementById("ccs_add_rfi_collab") !== null) {
 
-    let with_value_count = 5,
-      prev_input = 0,
-      deleteButtons = document.querySelectorAll("a.del");
-
-    for (var collab_fieldset = 5; collab_fieldset > 1; collab_fieldset--) {
-
-
-      let this_fieldset = document.querySelector(".collab_" + collab_fieldset),
-        name_box = document.getElementById("proj_collab_name_" + collab_fieldset);
-
-      if (name_box.value !== "") {
-        this_fieldset.classList.remove('ccs-dynaform-hidden');
-
-        if (collab_fieldset === 5) {
-          document.getElementById("ccs_collab_add").classList.add('ccs-dynaform-hidden');
-        }
-
-      } else {
-
-        this_fieldset.classList.add('ccs-dynaform-hidden');
-        with_value_count = collab_fieldset;
-      }
-
-    }
-
-    document.getElementById("ccs_collab_add").addEventListener('click', (e) => {
-      e.preventDefault();
-
-      document.querySelector(".collab_" + with_value_count).classList.remove("ccs-dynaform-hidden");
-
-      if (with_value_count > 2 ) {
-        prev_input = with_value_count - 1;
-        document.querySelector(".collab_"  + prev_input + " a.del").classList.add("ccs-dynaform-hidden");
-      }
-
-      with_value_count++;
-
-      if (with_value_count === 6) {
-        document.getElementById("ccs_collab_add").classList.add('ccs-dynaform-hidden');
-      }
-
-    });
-
-    // delete buttons
+    document.getElementById('potential-collaborator').classList.add('ccs-dynaform-hidden');
+   
+    let deleteButtons = document.querySelectorAll("a.del");
     deleteButtons.forEach((db) => {
-      db.addEventListener('click', (e) => {
-
-        e.preventDefault();
-
-        let target = db.href.replace(/^(.+\/)(\d{1,2})$/, "$2"),
-          prev_coll = Number(target) - 1,
-          target_fieldset = db.closest("fieldset");
-
-        target_fieldset.classList.add("ccs-dynaform-hidden");
-
-        document.getElementById('proj_collab_name_' + target).value = "";
-        document.getElementById('proj_collab_email_' + target).value = "";
-        document.getElementById('proj_collab_tel_' + target).value = "";
-
-        if (prev_coll > 1) {
-          document.querySelector('.collab_' + prev_coll + ' a.del').classList.remove("ccs-dynaform-hidden");
-        }
-
-        document.getElementById("ccs_collab_add").classList.remove('ccs-dynaform-hidden');
-        with_value_count--;
+      db.addEventListener('click', function (event) {
+        event.preventDefault();
+        let target = event.target.href.replace(/^(.+\/)(\d{1,2})$/, "$2"),
+        prev_coll = Number(target);
+        $.ajax({
+          url: "/rfi/delete-rfi-collaborator?id=" + prev_coll,
+          type: "POST",
+          contentType: "application/json"
+        }).done(function (result) {
+          console.log(result);
+          const parent_node = document.getElementById('collaborators-section');
+          const node_to_remove = document.getElementById("rfi_collaborator_" + result.id);
+          parent_node.removeChild(node_to_remove);
+          var userOp = document.createElement('option');
+          userOp.value = result.id;
+          userOp.text = result.fullname;
+          document.getElementById('rfi_collaborators').appendChild(userOp);
+        });
       });
     });
+
+    document.getElementById('rfi_collaborators').addEventListener('change', function (event) {
+      event.preventDefault();
+      if (event.target.value !== "0") {
+        console.log({msg: "/rfi/get-collaborator-detail?id=" + event.target.value})
+        $.ajax({
+          url: "/rfi/get-collaborator-detail?id=" + event.target.value,
+          type: "POST",
+          contentType: "application/json",
+          //data: JSON.stringify({ id: 2 })
+        }).done(function (result) {
+          document.getElementById('potential-collaborator').classList.remove('ccs-dynaform-hidden');
+          setUserDetails(result);
+        });
+      }
+
+    });
+
+    const setUserDetails = (user) => {
+      let div_email = document.getElementById('collab_email');
+      div_email.innerText = user.email;
+      let div_tel = document.getElementById('collab_tel');
+      div_tel.innerText = user.telephone;
+      let div_name = document.getElementById('collab_name');
+      div_name.innerText = user.fullname;
+    };
+
+    document.getElementById('ccs_collab_add').addEventListener('click', function (event) {
+      event.preventDefault();
+
+      $.ajax({
+        url: "/rfi/add-rfi-collaborator?id=" +  document.getElementById('rfi_collaborators').value,
+        type: "POST",
+        contentType: "application/json",
+      //data: JSON.stringify({ id: 2 })
+      }).done(function (result) {
+        let parentNode = document.getElementById('collaborators-section');
+      //clone the detail element 
+      let clone = document.querySelector('#potential-collaborator-detail').cloneNode(true);
+      clone.setAttribute('id', "rfi_collaborator_" + document.getElementById('rfi_collaborators').value);
+      let anchorTag = document.createElement('a');
+      anchorTag.href = document.getElementById('rfi_collaborators').value;
+      anchorTag.innerText = 'Remove';
+      anchorTag.classList.add('del', 'govuk-link', 'govuk-link--no-visited-state');
+      anchorTag.addEventListener('click', RemoveUser);
+      anchorTag.title = 'delete';
+      clone.prepend(anchorTag);
+      clone.classList.add('ccs-page-section');
+
+      // add it to parent div
+      parentNode.prepend(clone);
+      [...document.getElementById('rfi_collaborators').options]
+    .filter(opt_val => opt_val.value === result.id)
+    .forEach(opt_val => opt_val.remove());
+      document.getElementById('potential-collaborator').classList.add('ccs-dynaform-hidden');
+      });
+     
+    });
+
+    const RemoveUser = (event) => {
+       event.preventDefault();
+      let target = event.target.href.replace(/^(.+\/)(\d{1,2})$/, "$2"),
+      prev_coll = Number(target);
+      $.ajax({
+        url: "/rfi/delete-rfi-collaborator?id=" + prev_coll,
+        type: "POST",
+        contentType: "application/json"
+      }).done(function (result) {
+        const parent_node = document.getElementById('collaborators-section');
+        const node_to_remove = document.getElementById("rfi_collaborator_" + prev_coll);
+        parent_node.removeChild(node_to_remove);
+        var userOp = document.createElement('option');
+        userOp.value = result.id;
+        userOp.text = result.fullname;
+        document.getElementById('rfi_collaborators').appendChild(userOp);
+      });
+
+    }
 
   }
 
 });
 
-const ccsZvalidateTeamMems = (event) => {
-  event.preventDefault();
-
-  let fieldCheck = "",
-    errorStore = [],
-    longNum = ["zero", "one", "two", "three", "four", "five"];
-
-  for (var x = 1; x < 6; x++) {
-    let name_field = document.getElementById('proj_collab_name_' + x);
-
-    // console.log(name_field.classList.value);
-
-    if (name_field.closest("fieldset").classList.value.indexOf("ccs-dynaform-hidden") === -1) {
-      fieldCheck = ccsZvalidateWithRegex( "proj_collab_name_" + x, "Provide the name of team member " + longNum[x], /^[a-z| ]{3,}$/i );
-      if (fieldCheck !== true) errorStore.push(fieldCheck);
-
-      fieldCheck = ccsZvalidateWithRegex( "proj_collab_email_" + x, "Provide the email of team member " + longNum[x], /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ );
-      if (fieldCheck !== true) errorStore.push(fieldCheck);
-    }
-  }
-
-  if (errorStore.length === 0) document.forms["ccs_add_collab"].submit();
-  else ccsZPresentErrorSummary(errorStore);
-
-};
