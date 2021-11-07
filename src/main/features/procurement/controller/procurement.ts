eb9750d09de60f69bc838493cb2250a36b8fd991
ulysses <ulysses.alvarez@cognizant.com>
2021-11-07 17:03:27 +0000

    Refactored json data and template to accommodate pre-market engagement prototype.

--- src/main/features/procurement/controller/procurement.ts
+++ src/main/features/procurement/controller/procurement.ts
@@ -14,19 +14,19 @@ import { LoggTracer } from '../../../common/logtracer/tracer';
  * @param req 
  * @param res 
  */
-export const PROCUREMENT = async (req : express.Request, res : express.Response)=> {
-  const { lotId, agreementLotName } = req.query; 
-  var {SESSION_ID} = req.cookies; 
+export const PROCUREMENT = async (req: express.Request, res: express.Response) => {
+  const { lotId, agreementLotName } = req.query;
+  var { SESSION_ID } = req.cookies;
   const agreementId_session = req.session.agreement_id;
   const lotsURL = `/tenders/projects/agreements`;
   const eventTypesURL = `agreements/${agreementId_session}/lots/${lotId}/event-types`;
-  let appendData: any = { ...data, SESSION_ID, agreement_header: {number:agreementId_session} };
-  try { 
-    const {data: typesRaw} = await AgreementAPI.Instance.get(eventTypesURL);
+  let appendData: any = { ...data, SESSION_ID, agreement_header: { number: agreementId_session } };
+  try {
+    const { data: typesRaw } = await AgreementAPI.Instance.get(eventTypesURL);
     const types = typesRaw.map((typeRaw: any) => typeRaw.type);
- 
-    appendData = {types, ...appendData };
-    
+
+    appendData = { types, ...appendData };
+
     const elementCached = req.session.procurements.find((proc: any) => proc.defaultName.components.lotId === lotId);
     let procurement;
     if (!elementCached) {
@@ -34,39 +34,39 @@ export const PROCUREMENT = async (req : express.Request, res : express.Response)
         "agreementId": agreementId_session,
         "lotId": lotId
       }
-      const {data: procurementRaw} = await TenderApi.Instance(SESSION_ID).post(lotsURL, _body);
+      const { data: procurementRaw } = await TenderApi.Instance(SESSION_ID).post(lotsURL, _body);
       procurement = procurementRaw;
       req.session.procurements.push(procurement);
     }
     else {
       procurement = elementCached;
     }
-    req.session.lotId =  procurement['defaultName']['components']['lotId'];
+    req.session.lotId = procurement['defaultName']['components']['lotId'];
     req.session.project_name = procurement['defaultName']['name'];
     req.session.agreementLotName = agreementLotName;
     const agreementName = req.session.agreementName;
-    appendData = {...appendData, agreement_header: {name: agreementName, number: agreementId_session, lotId: lotId,  project_name: procurement['defaultName']['name'], agreementLotName: agreementLotName }};
+    appendData = { ...appendData, agreement_header: { name: agreementName, number: agreementId_session, lotId: lotId, project_name: procurement['defaultName']['name'], agreementLotName: agreementLotName } };
 
-  } catch(error) { 
-      console.log(error);
-      
-      delete error?.config?.['headers'];
-      let Logmessage = {
-        "Person_email": TokenDecoder.decoder(SESSION_ID),
-         "error_location": `${req.headers.host}${req.originalUrl}`,
-         "sessionId": "null",
-         "error_reason": "Tender agreement failed to be added",
-         "exception": error
-        }
-     let Log = new LogMessageFormatter(
-         Logmessage.Person_email, 
-         Logmessage.error_location, 
-         Logmessage.sessionId,
-         Logmessage.error_reason, 
-         Logmessage.exception
-         )
-         LoggTracer.errorTracer(Log, res);
-    }   
-    res.render('procurement', appendData);
-        
+  } catch (error) {
+    console.log(error);
+
+    delete error?.config?.['headers'];
+    let Logmessage = {
+      "Person_email": TokenDecoder.decoder(SESSION_ID),
+      "error_location": `${req.headers.host}${req.originalUrl}`,
+      "sessionId": "null",
+      "error_reason": "Tender agreement failed to be added",
+      "exception": error
+    }
+    let Log = new LogMessageFormatter(
+      Logmessage.Person_email,
+      Logmessage.error_location,
+      Logmessage.sessionId,
+      Logmessage.error_reason,
+      Logmessage.exception
+    )
+    LoggTracer.errorTracer(Log, res);
+  }
+  res.render('procurement', appendData);
+
 }
