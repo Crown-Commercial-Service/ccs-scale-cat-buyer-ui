95687a7291d14f95567f74821a0b0a5f90fcd900
ulysses <ulysses.alvarez@cognizant.com>
2021-11-07 15:22:09 +0000

    rebase refactoring

--- src/main/common/util/fetch/agreementservice/agreementsApiInstance.ts
+++ src/main/common/util/fetch/agreementservice/agreementsApiInstance.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import * as axios from 'axios';
 
@@ -7,6 +8,16 @@ export class AgreementAPI {
         baseURL: process.env['AGREEMENTS_SERVICE_API_URL'],
         headers: {
             'Content-Type': 'application/json'
+=======
+import * as axios from 'axios'
+
+export class AgreementAPI {
+
+    static Instance : axios.AxiosInstance = axios.default.create({
+        baseURL: process.env['AGREEMENTS_SERVICE_API_URL'],
+        headers: {
+        'Content-Type': 'application/json'
+>>>>>>> bc1a6d01 (rebase refactoring)
         }
     })
 
--- src/main/features/rfi/controller/index.ts
+++ src/main/features/rfi/controller/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { GET_TASKLIST } from './tasklist'
 import { GET_TYPE, POST_TYPE } from './type'
 import { GET_ONLINE_TASKLIST } from './onlinetasklist'
@@ -6,6 +7,13 @@ import { GET_UPLOAD_DOC } from './upload'
 import { GET_NAME_PROJECT, POST_NAME_PROJECT } from './nameAProject'
 import { GET_LEAD_PROCUREMENT } from './leadProcurement'
 import { GET_ADD_COLLABORATOR } from './addcollaborator'
+=======
+import {GET_TASKLIST} from './tasklist'
+import {GET_TYPE, POST_TYPE} from './type'
+import {GET_ONLINE_TASKLIST} from './onlinetasklist'
+import {GET_QUESTIONS, POST_QUESTION} from './questions'
+import { GET_UPLOAD_DOC } from './upload';
+>>>>>>> bc1a6d01 (rebase refactoring)
 
 
 export var associatedViews = {
@@ -17,9 +25,12 @@ export var associatedViews = {
     GET_TYPE,
     GET_QUESTIONS,
     GET_UPLOAD_DOC,
+<<<<<<< HEAD
     GET_NAME_PROJECT,
     GET_LEAD_PROCUREMENT,
     GET_ADD_COLLABORATOR,
+=======
+>>>>>>> bc1a6d01 (rebase refactoring)
 
 
 
