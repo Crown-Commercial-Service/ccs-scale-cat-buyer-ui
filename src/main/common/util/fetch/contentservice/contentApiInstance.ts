55c38550389955821cf65ac12eab47e0666832cb
Prathap Mathiyalagan <prathap.mathiyalagan@cognizant.com>
2021-11-09 22:29:33 +0000

    added timeout

--- src/main/common/util/fetch/contentservice/contentApiInstance.ts
+++ src/main/common/util/fetch/contentservice/contentApiInstance.ts
@@ -3,12 +3,10 @@ import * as axios from 'axios';
 export class contentAPI {
 
     static Instance : axios.AxiosInstance = axios.default.create({
-
         baseURL: process.env['AGREEMENTS_SERVICE_API_URL'],
+        timeout: 1000 * 5, // Wait for 5 seconds
         headers: {
         'Content-Type': 'application/json'
         }
-
     })
-
 }
