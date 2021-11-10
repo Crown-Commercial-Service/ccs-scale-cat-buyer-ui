80d94572f892e4044d111cdf52ab6313a34b0bc7
Abdul Daim <89971948+daimdev@users.noreply.github.com>
2021-11-10 11:34:55 +0000

    Reverted authentication bit, added rfi journey, modified rfi components and changed flow auth of conclave

--- src/main/common/middlewares/oauthservice/receiver.ts
+++ src/main/common/middlewares/oauthservice/receiver.ts
@@ -15,7 +15,7 @@ import * as jwtDecoder from 'jsonwebtoken';
  * @param res 
  * @param next 
  */
-export const CREDENTAILS_FETCH_RECEIVER = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+export const CREDENTAILS_FETCH_RECEIVER = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     let {
         code, state
     } = req.query;
@@ -32,43 +32,68 @@ export const CREDENTAILS_FETCH_RECEIVER = async (req: express.Request, res: expr
             "redirect_uri": process.env.CAT_URL + '/receiver',
         }
         auth_credentails = qs.stringify(auth_credentails)
-        //@ Grant Authorization with the token to re-direct to the callback page     
+        //@ Grant Authorization with the token to re-direct to the callback page
+        let PostAuthCrendetails = Oauth_Instance.Instance.post(Oauth_check_endpoint, auth_credentails);
+        //@ set the cookies for SESSION_ID and state with an expiration time
+        PostAuthCrendetails.then((data) => {
 
-        try {
-            let PostAuthCrendetails = await Oauth_Instance.Instance.post(Oauth_check_endpoint, auth_credentails);
-            let data = PostAuthCrendetails?.data
-            let containedData = data;
-            let { access_token, session_state } = containedData;
+            let containedData = data?.data;
+            let {
+                access_token, session_state
+            } = containedData;
             let AuthCheck_Instance = Oauth_Instance.TokenCheckInstance(access_token);
-            let check_token_validation = await AuthCheck_Instance.post('');
-            let auth_status_check = check_token_validation?.['data'];
-            if (auth_status_check) {
-                let cookieExpiryTime = Number(config.get('Session.time'));
-                cookieExpiryTime = cookieExpiryTime * 60 * 1000;  //milliseconds
-                let timeforcookies = cookieExpiryTime
-                res.cookie(cookies.sessionID, access_token, {
-                    maxAge: Number(timeforcookies),
-                    httpOnly: true
-                })
-                res.cookie(cookies.state, state, {
-                    maxAge: Number(timeforcookies),
-                    httpOnly: true
-                })
-                let userSessionInformation = jwtDecoder.decode(access_token, { complete: true });
-                req.session['isAuthenticated'] = true;
-                req.session['access_token'] = access_token;
-                req.session['user'] = userSessionInformation;
-                req.session['userServerSessionID'] = session_state;
-                req.session['procurements'] = [];
-                req.session['agreement_id'] = "";
-                req.session['agreementName'] = "";
+            let check_token_validation = AuthCheck_Instance.post('');
+            check_token_validation.then(data => {
+                let auth_status_check = data?.['data'];
 
-                next();
-            } else {
-                res.redirect('/oauth/logout')
-            }
+                if (auth_status_check) {
+                    let cookieExpiryTime = Number(config.get('Session.time'));
+                    cookieExpiryTime = cookieExpiryTime * 60 * 1000;  //milliseconds
+                    let timeforcookies = cookieExpiryTime
 
-        } catch (error) {
+                    res.cookie(cookies.sessionID, access_token, {
+                        maxAge: Number(timeforcookies),
+                        httpOnly: true
+                    })
+
+                    res.cookie(cookies.state, state, {
+                        maxAge: Number(timeforcookies),
+                        httpOnly: true
+                    })
+
+                    let userSessionInformation = jwtDecoder.decode(access_token, { complete: true });
+                    req.session['isAuthenticated'] = true;
+                    req.session['access_token'] = access_token;
+                    req.session['user'] = userSessionInformation;
+                    req.session['userServerSessionID'] = session_state;
+                    req.session['procurements'] = [];
+                    req.session['agreement_id'] = "";
+                    req.session['agreementName'] = "";
+
+                    next();
+                } else {
+                    res.redirect('/oauth/logout')
+                }
+            }).catch(error => {
+                delete error?.config?.['headers'];
+                let Logmessage = {
+                    "Person_email": 'null',
+                    "error_location": `${req.headers.host}${req.originalUrl}`,
+                    "sessionId": "null",
+                    "error_reason": "Conclave authentication flow error",
+                    "exception": error
+                }
+                let Log = new LogMessageFormatter(
+                    Logmessage.Person_email,
+                    Logmessage.error_location,
+                    Logmessage.sessionId,
+                    Logmessage.error_reason,
+                    Logmessage.exception
+                )
+                LoggTracer.errorTracer(Log, res);
+
+            });
+        }).catch(error => {
             delete error?.config?.['headers'];
             let Logmessage = {
                 "Person_email": 'null',
@@ -85,7 +110,7 @@ export const CREDENTAILS_FETCH_RECEIVER = async (req: express.Request, res: expr
                 Logmessage.exception
             )
             LoggTracer.errorTracer(Log, res);
-        }
-
+        })
     }
 }
+
