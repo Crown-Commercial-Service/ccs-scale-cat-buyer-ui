14a71ca172fa3ea71dfeef653207f4d17fa28b71
Annamalai <629210@cognizant.com>
2021-11-24 15:56:50 +0000

    Client_side_cosmetic_validation

--- src/main/public/assets/scripts/validations/rfi_acronyms.js
+++ src/main/public/assets/scripts/validations/rfi_acronyms.js
@@ -29,20 +29,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById("ccs_rfiTerm_add").addEventListener('click', (e) => {
       e.preventDefault();
+      errorStore = emptyFieldCheck();
+      if(errorStore.length == 0)
+      {   
 
-      document.querySelector(".acronym_" + with_value_count).classList.remove("ccs-dynaform-hidden");
+        document.querySelector(".acronym_" + with_value_count).classList.remove("ccs-dynaform-hidden");
 
-      if (with_value_count > 2 ) {
-        prev_input = with_value_count - 1;
-        document.querySelector(".acronym_"  + prev_input + " a.del").classList.add("ccs-dynaform-hidden");
+        if (with_value_count > 2 ) {
+          prev_input = with_value_count - 1;
+          document.querySelector(".acronym_"  + prev_input + " a.del").classList.add("ccs-dynaform-hidden");
+        }
+
+        with_value_count++;
+
+        if (with_value_count === 11) {
+          document.getElementById("ccs_rfiTerm_add").classList.add('ccs-dynaform-hidden');
+        }
       }
-
-      with_value_count++;
-
-      if (with_value_count === 11) {
-        document.getElementById("ccs_rfiTerm_add").classList.add('ccs-dynaform-hidden');
-      }
-
+      else ccsZPresentErrorSummary(errorStore);
     });
 
     // delete buttons
@@ -74,14 +78,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-const ccsZvalidateAcronyms = (event) => {
-  event.preventDefault();
-
+const emptyFieldCheck = () => {
   let fieldCheck = "",
-    errorStore = [],
-    longNum = ["0", "1", "2", "3", "4", "5"];
+    errorStore = [];
 
-  for (var x = 1; x < 6; x++) {
+  for (var x = 1; x < 11; x++) {
     let term_field = document.getElementById('rfi_term_' + x);
     let definition_field = document.getElementById("rfi_term_definition_" + x);
 
@@ -101,6 +102,12 @@ const ccsZvalidateAcronyms = (event) => {
       }
     }
   }
+  return errorStore;
+}
+const ccsZvalidateAcronyms = (event) => {
+  event.preventDefault();
+
+  errorStore = emptyFieldCheck();
 
   if (errorStore.length === 0) document.forms["ccs_rfi_acronyms_form"].submit();
   else ccsZPresentErrorSummary(errorStore);
--- src/main/public/assets/scripts/validations/rfi_questions.js
+++ src/main/public/assets/scripts/validations/rfi_questions.js
@@ -55,9 +55,13 @@ document.addEventListener('DOMContentLoaded', () => {
           prev_box = Number(target) - 1;
         document.getElementById('rfi_question_' + target).value = "";
         document.getElementById('rfi_question_' + target).classList.add("ccs-dynaform-hidden");
+        let parentNode = document.querySelector('label[for=rfi_question_' + target + ']').parentNode;
+        parentNode.removeChild(document.getElementById("rfi_question_" + target + '-error'))
+        parentNode.classList.remove("govuk-form-group--error");
+        parentNode.children["rfi_question_" + target ].classList.remove("govuk-input--error");
         //document.getElementById('rfi_question_' + target + '-error').parentNode.removeChild(document.getElementById('rfi_question_' + target + '-error'));
         document.querySelector('label[for=rfi_question_' + target + ']').classList.add("ccs-dynaform-hidden");
-
+        
         if (prev_box > 1) {
           document.querySelector('label[for=rfi_question_' + prev_box + '] a.del').classList.remove("ccs-dynaform-hidden");
         }
@@ -80,7 +84,6 @@ const ccsZvalidateRfIQuestions = (event) => {
   //if (event_typ !== "Request for Information") {
     fieldCheck = ccsZvalidateWithRegex( "rfi_question_1", "Enter question 1", /\w+/);
     if (fieldCheck !== true) errorStore.push(fieldCheck);
-
     for (var i = 2; i < 11; i++) {
       if (!document.getElementById("rfi_question_" + i).classList.contains('ccs-dynaform-hidden')) {
         fieldCheck = ccsZvalidateWithRegex( "rfi_question_" + i, "Enter question " + i, /\w+/);
