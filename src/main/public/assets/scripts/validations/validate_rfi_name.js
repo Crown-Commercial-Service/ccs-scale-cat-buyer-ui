41243d8f310b08225f354f7a782c6f088de6eeed
Prathap Mathiyalagan <prathap.mathiyalagan@cognizant.com>
2021-11-25 20:23:06 +0000

    added server side validation for name your project

--- src/main/public/assets/scripts/validations/validate_rfi_name.js
+++ src/main/public/assets/scripts/validations/validate_rfi_name.js
@@ -4,7 +4,7 @@ const ccsZvalidateProjectName = (event) => {
     let fieldCheck = "",
     errorStore = [];
 
-    fieldCheck = ccsZvalidateWithRegex( "rfi_projLongName", "Your project must have a name. ", /^.+$/ );
+    fieldCheck = ccsZvalidateWithRegex( "rfi_projLongName", "Your project must have a name.", /^.+$/ );
     if (fieldCheck !== true) errorStore.push(fieldCheck);
 
     if (errorStore.length === 0) document.forms["ccs_project_name_form"].submit();
