4af5b632cadd87a4497b61a90c2ef190ab543f30
Cesar <cesarperezfernandez87@gmail.com>
2021-12-01 17:15:08 +0100

    trying to see traces

--- src/main/features/rfi/util/fetch/dyanmicframeworkInstance.ts
+++ src/main/features/rfi/util/fetch/dyanmicframeworkInstance.ts
@@ -1,15 +1,14 @@
-import * as axios from 'axios'
+import * as axios from 'axios';
 
 export class DynamicFrameworkInstance {
-
-    static Instance = (SESSION_ID : string) => {
-
-            return  axios.default.create({
-                baseURL: process.env.TENDERS_SERVICE_API_URL,
-                headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${SESSION_ID}`
-                }
-            })
-    }
+  static Instance = (SESSION_ID: string) => {
+    return axios.default.create({
+      baseURL: process.env.TENDERS_SERVICE_API_URL,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${SESSION_ID}`,
+        'Cache-Control': 'must-revalidate, no-store, no-cache, private',
+      },
+    });
+  };
 }
