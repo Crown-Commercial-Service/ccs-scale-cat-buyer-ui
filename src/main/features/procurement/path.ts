bde3a86706d12f37b7cd1e3a8776e3483b829946
Prathap Mathiyalagan <89925857+prathap-cognizant@users.noreply.github.com>
2021-12-01 12:28:31 +0000

    Revert "Added functionality for storing hitted URLs and then redirect after A…"

--- src/main/features/procurement/path.ts
+++ src/main/features/procurement/path.ts
@@ -4,13 +4,11 @@ import { PROCUREMENT_PATHS } from './model/procurement';
 import { Application } from 'express';
 import { ContentFetchMiddleware } from '../../common/middlewares/menu-contentservice/contentservice'
 import { AgreementDetailsFetchMiddleware } from '../../common/middlewares/agreementservice/agreementdetailsfetch'
-import {BookMarkMiddleware} from '../../common/middlewares/bookmarked/bookmark-redirect'
+
 
 export default function (app: Application): void {
-    const {BookMarkStorer} = BookMarkMiddleware;
     // This is the reciever callback after getting the token
     app.get(PROCUREMENT_PATHS.PROCUREMENT, [
         ContentFetchMiddleware.FetchContents,
-        BookMarkStorer,
         AUTH, AgreementDetailsFetchMiddleware.FetchAgreements], associatedViews.PROCUREMENT);
 }
