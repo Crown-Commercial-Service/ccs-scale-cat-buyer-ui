7799f93383c340fce244c4100cc902dbe976f965
ulysses <ulysses.alvarez@cognizant.com>
2021-12-02 23:17:27 +0000

    uncomment no-cache in app.

--- src/main/app.ts
+++ src/main/app.ts
@@ -7,27 +7,25 @@ import * as path from 'path';
 import favicon from 'serve-favicon';
 import { Nunjucks } from './modules/nunjucks';
 const { setupDev } = require('./setup/development');
-import i18next from 'i18next'
+import i18next from 'i18next';
 const env = process.env.NODE_ENV || 'development';
 const developmentMode = env === 'development';
-import { NotFoundError } from './errors/errors'
-import fs from 'fs'
+import { NotFoundError } from './errors/errors';
+import fs from 'fs';
 export const app = express();
-import glob from 'glob'
-import { routeExceptionHandler } from './setup/routeexception'
-import { RedisInstanceSetup } from './setup/redis'
-import {fileUploadSetup} from './setup/fileupload'
-
+import glob from 'glob';
+import { routeExceptionHandler } from './setup/routeexception';
+import { RedisInstanceSetup } from './setup/redis';
+import { fileUploadSetup } from './setup/fileupload';
 
 app.locals.ENV = env;
 
-
 // setup logging of HTTP requests
 /**
- * @env Local variables 
+ * @env Local variables
  */
-let checkforenvFile = fs.existsSync('.env')
-import { localEnvariables } from './setup/envs'
+let checkforenvFile = fs.existsSync('.env');
+import { localEnvariables } from './setup/envs';
 if (checkforenvFile) {
   localEnvariables(app);
 }
@@ -47,25 +45,23 @@ new Helmet(config.get('security')).enableFor(app);
 
 app.use(Express.accessLogger());
 app.use(favicon(path.join(__dirname, '/public/assets/images/favicon.ico')));
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('src/main/public'));
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     'Cache-Control',
-//     'no-cache, max-age=0, must-revalidate, no-store',
-//   );
-//   next();
-// });
-app.enable('trust proxy')
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, max-age=0, must-revalidate, no-store');
+  next();
+});
+app.enable('trust proxy');
 
 /**
  * @Routable path getting content from default.json
  */
-let featureRoutes: Array<Object> = config.get('featureDir')
+let featureRoutes: Array<Object> = config.get('featureDir');
 featureRoutes?.forEach((aRoute: any) => {
-  glob.sync(__dirname + aRoute?.['path'])
+  glob
+    .sync(__dirname + aRoute?.['path'])
     .map((filename: string) => require(filename))
     .forEach((route: any) => route.default(app));
 });
@@ -73,19 +69,13 @@ featureRoutes?.forEach((aRoute: any) => {
 /**
  * @developementEnvironment
  *  Setting up development environment
- *  
+ *
  */
 setupDev(app, developmentMode);
 
 /**
  * @ExceptionHandler
- *  All error Handler Routes 
- *  
+ *  All error Handler Routes
+ *
  */
-routeExceptionHandler(
-  app,
-  NotFoundError,
-  logger,
-  env
-)
-
+routeExceptionHandler(app, NotFoundError, logger, env);
