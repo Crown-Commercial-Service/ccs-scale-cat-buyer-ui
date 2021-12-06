3902ee601b270d23d2ad17d1bd0869a58387c899
ulysses <ulysses.alvarez@cognizant.com>
2021-12-06 13:53:51 +0000

    refactoring all folders for eoi

---
+++ src/main/features/eoi/eoi/path.ts
@@ -0,0 +1,117 @@
+import { Application } from 'express';
+import { EOI_PATHS } from './model/eoiconstant';
+import { associatedViews } from './controller/index';
+import * as apisource from '../../resources/content/eoi/template.json';
+import { AgreementDetailsFetchMiddleware } from '../../common/middlewares/agreementservice/agreementdetailsfetch';
+import { AUTH } from '../../common/middlewares/oauthservice/authstatecheck';
+import { ContentFetchMiddleware } from '../../common/middlewares/menu-contentservice/contentservice';
+import { PreMarketEngagementMiddleware } from '../../common/middlewares/premarketservice/premarketengagement';
+
+export default function (app: Application): void {
+  /**
+   *
+   * @GET : Get Routes of EOI
+   * @summary: provide all the respective associated view to the certain routes
+   */
+  // @GET '/eoi/eoi-task-list'
+  app.get(
+    EOI_PATHS.GET_TASKLIST,
+    [
+      ContentFetchMiddleware.FetchContents,
+      AUTH,
+      AgreementDetailsFetchMiddleware.FetchAgreements,
+      PreMarketEngagementMiddleware.PutPremarket,
+    ],
+    associatedViews.GET_TASKLIST,
+  );
+
+  //  @GET '/eoi/type'
+  app.get(
+    EOI_PATHS.GET_TYPE,
+    [ContentFetchMiddleware.FetchContents, AUTH, AgreementDetailsFetchMiddleware.FetchAgreements],
+    associatedViews.GET_TYPE,
+  );
+
+  //  @GET '/eoi/online-task-list'
+  app.get(
+    EOI_PATHS.GET_ONLINE_TASKLIST,
+    [ContentFetchMiddleware.FetchContents, AUTH, AgreementDetailsFetchMiddleware.FetchAgreements],
+    associatedViews.GET_ONLINE_TASKLIST,
+  );
+
+  //  @GET '/eoi/questions'
+  app.get(
+    EOI_PATHS.GET_QUESTIONS,
+    [ContentFetchMiddleware.FetchContents, AUTH, AgreementDetailsFetchMiddleware.FetchAgreements],
+    associatedViews.GET_QUESTIONS,
+  );
+
+  //  @GET '/eoi/upload-doc'
+  app.get(
+    EOI_PATHS.GET_UPLOAD_DOC,
+    [ContentFetchMiddleware.FetchContents, AUTH, AgreementDetailsFetchMiddleware.FetchAgreements],
+    associatedViews.GET_UPLOAD_DOC,
+  );
+
+  //@GET name your projects
+
+  app.get(
+    EOI_PATHS.GET_NAME_YOUR_PROJECT,
+    [ContentFetchMiddleware.FetchContents, AUTH, AgreementDetailsFetchMiddleware.FetchAgreements],
+    associatedViews.GET_NAME_PROJECT,
+  );
+
+  //@GET '/eoi/add-collaborators'
+  app.get(
+    EOI_PATHS.GET_ADD_COLLABORATOR,
+    [ContentFetchMiddleware.FetchContents, AUTH, AgreementDetailsFetchMiddleware.FetchAgreements],
+    associatedViews.GET_ADD_COLLABORATOR,
+  );
+
+  //@GET '/eoi/procurement-lead'
+  app.get(
+    EOI_PATHS.GET_LEAD_PROCUEMENT,
+    [ContentFetchMiddleware.FetchContents, AUTH, AgreementDetailsFetchMiddleware.FetchAgreements],
+    associatedViews.GET_LEAD_PROCUREMENT,
+  );
+
+  app.get(EOI_PATHS.GET_USER_PROCUREMENT, [AUTH], associatedViews.GET_USER_PROCUREMENT);
+
+  //@GET "/eoi/upload-doc/remove"
+  app.get(EOI_PATHS.GET_REMOVE_FILE, AUTH, associatedViews.GET_REMOVE_FILES);
+
+  /**
+   *
+   * @POST : POST Routes of EOI
+   * @summary: provide all the respective associated view to the certain routes
+   */
+
+  // api
+  app.get('/api/template', (req, res) => res.json(apisource));
+
+  //@POST '/eoi/type
+  app.post(EOI_PATHS.POST_TYPE_TYPE, AUTH, associatedViews.POST_TYPE);
+
+  //@POST '/eoi/questionnaire'
+  app.post(EOI_PATHS.POST_QUESTIONS_QUESTIONNAIRE, AUTH, associatedViews.POST_QUESTION);
+
+  //@POST '/eoi/name'
+  app.post(EOI_PATHS.POST_PROJECT_NAME, AUTH, associatedViews.POST_NAME_PROJECT);
+
+  //@POST '/eoi/get-collaborator-detail'
+  app.post(EOI_PATHS.POST_ADD_COLLABORATOR, AUTH, associatedViews.POST_ADD_COLLABORATOR);
+
+  app.post(EOI_PATHS.PUT_LEAD_PROCUREMENT, AUTH, associatedViews.PUT_LEAD_PROCUREMENT);
+
+  //@POST '/eoi/proceed-collaborators'
+  app.post(EOI_PATHS.POST_PROCEED_COLLABORTORS, AUTH, associatedViews.POST_PROCEED_COLLABORATORS);
+
+  //@POST '/eoi/get-collaborator-detail/js-enabled'
+  app.post(EOI_PATHS.POST_ADD_COLLABORATOR_JSENABLED, AUTH, associatedViews.POST_ADD_COLLABORATOR_JSENABLED);
+
+  //@POST '/eoi/add-collaborator-detail'
+  app.post(EOI_PATHS.POST_ADD_COLLABORATOR_TO_JAGGER, AUTH, associatedViews.POST_ADD_COLLABORATOR_TO_JAGGER);
+
+  //@POST "eoi/upload-doc"
+  app.post(EOI_PATHS.POST_UPLOAD_DOC, AUTH, associatedViews.POST_UPLOAD_DOC);
+}
---
+++ src/main/resources/content/eoi/controller/onlinetasklist.ts
@@ -0,0 +1,92 @@
+//@ts-nocheck
+import * as express from 'express';
+import { DynamicFrameworkInstance } from '../util/fetch/dyanmicframeworkInstance';
+import fileData from '../../../resources/content/eoi/eoionlineTaskList.json';
+import { operations } from '../../../utils/operations/operations';
+import { ErrorView } from '../../../common/shared/error/errorView';
+import { TokenDecoder } from '../../../common/tokendecoder/tokendecoder';
+import { LoggTracer } from '../../../common/logtracer/tracer';
+
+// eoi TaskList
+/**
+ *
+ * @param req
+ * @param res
+ * @GETController
+ */
+export const GET_ONLINE_TASKLIST = async (req: express.Request, res: express.Response) => {
+  if (
+    operations.isUndefined(req.query, 'agreement_id') ||
+    operations.isUndefined(req.query, 'proc_id') ||
+    operations.isUndefined(req.query, 'event_id')
+  ) {
+    res.redirect(ErrorView.notfound);
+  } else {
+    const { agreement_id, proc_id, event_id } = req.query;
+    const { SESSION_ID } = req.cookies;
+    const baseURL: any = `/tenders/projects/${proc_id}/events/${event_id}/criteria`;
+    try {
+      const fetch_dynamic_api = await DynamicFrameworkInstance.Instance(SESSION_ID).get(baseURL);
+      const fetch_dynamic_api_data = fetch_dynamic_api?.data;
+      const extracted_criterion_based = fetch_dynamic_api_data?.map((criterian: any) => criterian?.id);
+      let criterianStorage: any = [];
+      for (const aURI of extracted_criterion_based) {
+        const criterian_bas_url = `/tenders/projects/${proc_id}/events/${event_id}/criteria/${aURI}/groups`;
+        const fetch_criterian_group_data = await DynamicFrameworkInstance.Instance(SESSION_ID).get(criterian_bas_url);
+        const criterian_array = fetch_criterian_group_data?.data;
+        const rebased_object_with_requirements = criterian_array?.map((anItem: any) => {
+          const object = anItem;
+          object['criterianId'] = aURI;
+          return object;
+        });
+        criterianStorage.push(rebased_object_with_requirements);
+      }
+      criterianStorage = criterianStorage.flat();
+      const sorted_ascendingly = criterianStorage
+        .map((aCriterian: any) => {
+          const object = aCriterian;
+          object.OCDS['id'] = aCriterian.OCDS['id']?.split('Group ').join('');
+          return object;
+        })
+        .sort(
+          (current_cursor: any, iterator_cursor: any) =>
+            Number(current_cursor.OCDS['id']) - Number(iterator_cursor.OCDS['id']),
+        )
+        .map((aCriterian: any) => {
+          const object = aCriterian;
+          object.OCDS['id'] = `Group ${aCriterian.OCDS['id']}`;
+          return object;
+        });
+      const select_default_data_from_fetch_dynamic_api = sorted_ascendingly;
+      const lotId = req.session?.lotId;
+      const agreementLotName = req.session.agreementLotName;
+      const display_fetch_data = {
+        data: select_default_data_from_fetch_dynamic_api,
+        agreement_id: agreement_id,
+        file_data: fileData,
+        proc_id: proc_id,
+        event_id: event_id,
+        lotId,
+        agreementLotName,
+        relatedTitle: 'Related content',
+        lotURL:
+          '/agreement/lot?agreement_id=' +
+          req.session.agreement_id +
+          '&lotNum=' +
+          req.session.lotId.replace(/ /g, '%20'),
+        lotText: req.session.agreementName + ', ' + req.session.agreementLotName,
+      };
+      res.render('onlinetasklistEoi', display_fetch_data);
+    } catch (error) {
+      LoggTracer.errorLogger(
+        res,
+        error,
+        `${req.headers.host}${req.originalUrl}`,
+        null,
+        TokenDecoder.decoder(SESSION_ID),
+        'Tenders Service Api cannot be connected',
+        true,
+      );
+    }
+  }
+};
