cab4b0430fb5604d78c43e44f5160e0628ab6971
Prathap Mathiyalagan <prathap.mathiyalagan@cognizant.com>
2021-12-09 15:25:52 +0000

    Fix for SCAT-2743

--- src/main/features/agreement/controller/selectedAgreement.ts
+++ src/main/features/agreement/controller/selectedAgreement.ts
@@ -1,6 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-nocheck
 import * as express from 'express'
 import { ReleatedContent } from '../model/related-content'
+import { AgreementAPI } from '../../../common/util/fetch/agreementservice/agreementsApiInstance'
 
 /**
  * 
@@ -9,16 +11,20 @@ import { ReleatedContent } from '../model/related-content'
  * @param req 
  * @param res 
  */
-export const SELECTED_AGREEMENT = (req: express.Request, res: express.Response) => {
+export const SELECTED_AGREEMENT = async (req: express.Request, res: express.Response) => {
   const { lotId, agreementLotName, agreementId, agreementName } = req.query;
+
+  const BaseUrlAgreement = `/agreements/${agreementId}/lots/${lotId}`
+  const { data: retrieveAgreementLot } = await AgreementAPI.Instance.get(BaseUrlAgreement)
+
   req.session.agreement_id = agreementId;
-  req.session.agreementLotName = agreementLotName
+  req.session.agreementLotName = retrieveAgreementLot.name
   req.session.agreementName = agreementName
   req.session.lotId = lotId
 
   const releatedContent: ReleatedContent = new ReleatedContent();
   releatedContent.name = agreementName
-  releatedContent.lotName = agreementLotName
+  releatedContent.lotName = retrieveAgreementLot.name
   releatedContent.lotUrl = "/agreement/lot?agreement_id="+agreementId+"&lotNum="+lotId.replace(/ /g,"%20");
   releatedContent.title = 'Releated Content'
   req.session.releatedContent = releatedContent
