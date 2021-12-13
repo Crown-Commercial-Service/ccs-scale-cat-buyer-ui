fcff3272a8de03bd6c662edc5e3675133a21b2ce
ulysses <ulysses.alvarez@cognizant.com>
2021-12-13 16:58:26 +0000

    fixes for Done and Todo status.

--- src/main/utils/statusStepsDataFilter.ts
+++ src/main/utils/statusStepsDataFilter.ts
@@ -1,10 +1,14 @@
-enum StepState {
-  Optional = 'Optional',
-  NotStarted = 'Not started',
-  CannotStartYet = 'Cannot start yet',
-  InProgress = 'In progress',
-  Completed = 'Completed',
-}
+export const StepState = {
+  Optional: { key: 'Optional', value: 'Optional' },
+  NotStarted: { key: 'Not started', value: 'To do' },
+  CannotStartYet: { key: 'Cannot start yet', value: 'Cannot start yet' },
+  InProgress: { key: 'In progress', value: 'In progress' },
+  Completed: { key: 'Completed', value: 'Done' },
+};
+export const getValue = (stateKey: any) => {
+  const elemState: any = Object.keys(StepState).find((elem: any) => elem.key === stateKey);
+  return elemState.value;
+};
 
 export function statusStepsDataFilter(data: any, steps: any, type: string) {
   const { events } = data;
@@ -22,16 +26,17 @@ export function statusStepsDataFilter(data: any, steps: any, type: string) {
   events.forEach((event: any) => {
     event.eventTask.forEach((eventTask: any) => {
       const stepInfo = stepsByType[accum];
+
       if (stepInfo) {
-        const key = [
-          StepState.NotStarted,
-          StepState.CannotStartYet,
-          StepState.InProgress,
-          StepState.Completed,
+        const keyMap = [
+          StepState.NotStarted.key,
+          StepState.CannotStartYet.key,
+          StepState.InProgress.key,
+          StepState.Completed.key,
         ].includes(stepInfo.state)
           ? 'status'
           : 'required';
-        eventTask[key] = stepInfo.state;
+        eventTask[keyMap] = getValue(stepInfo.state);
       }
       accum = accum + 1;
     });
