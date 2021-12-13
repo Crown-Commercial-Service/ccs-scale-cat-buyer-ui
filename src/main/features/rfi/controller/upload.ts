aed42e1eca8dcd14b7d66c15c54698d58c2704a1
ulysses <ulysses.alvarez@cognizant.com>
2021-12-13 11:19:27 +0000

    fix for RFI upload.ts file

--- src/main/features/rfi/controller/upload.ts
+++ src/main/features/rfi/controller/upload.ts
@@ -1,6 +1,12 @@
 //@ts-nocheck
 import * as express from 'express';
 import * as cmsData from '../../../resources/content/RFI/offline-doc.json';
+import { DynamicFrameworkInstance } from '../util/fetch/dyanmicframeworkInstance';
+import { LoggTracer } from '../../../common/logtracer/tracer';
+import { TokenDecoder } from '../../../common/tokendecoder/tokendecoder';
+import { LogMessageFormatter } from '../../../common/logtracer/logmessageformatter';
+import FormData from 'form-data';
+import fileSystem from 'fs';
 
 let tempArray = [];
 
@@ -12,11 +18,60 @@ let tempArray = [];
  * @GETController
  */
 
-export const GET_UPLOAD_DOC: express.Handler = (req: express.Request, res: express.Response) => {
+export const GET_UPLOAD_DOC: express.Handler = async (req: express.Request, res: express.Response) => {
   const lotId = req.session?.lotId;
+  const { SESSION_ID } = req.cookies;
   const agreementLotName = req.session.agreementLotName;
-  const windowAppendData = { lotId, agreementLotName, data: cmsData, files: tempArray };
-  res.render('uploadDocument', windowAppendData);
+  const ProjectId = req.session['projectId'];
+  const EventId = req.session['eventId'];
+  const { file_id, name } = req.query;
+
+  if (file_id !== undefined) {
+    const FileDownloadURL = `/tenders/projects/${ProjectId}/events/${EventId}/documents/${file_id}`;
+    const FetchDocuments = await DynamicFrameworkInstance.file_dowload_Instance(SESSION_ID).get(FileDownloadURL, {
+      responseType: 'blob',
+    });
+    const file = FetchDocuments;
+    const fileName = file.headers['content-disposition'].split('filename=')[1].split('"').join('');
+    const type = file.headers['content-type'];
+
+    const directory = '/uploads/temporary/' + fileName;
+
+    const fileData = file.data.toString('utf-8');
+
+    fileSystem.appendFile(directory, fileData, function (err) {
+      if (err) throw err;
+      console.log('File is created successfully.');
+      const filef = directory;
+
+      res.download(filef);
+    });
+  }
+
+  try {
+    const FileuploadBaseUrl = `/tenders/projects/${ProjectId}/events/${EventId}/documents`;
+    const FetchDocuments = await DynamicFrameworkInstance.Instance(SESSION_ID).get(FileuploadBaseUrl);
+    const FETCH_FILEDATA = FetchDocuments.data;
+    const windowAppendData = { lotId, agreementLotName, data: cmsData, files: FETCH_FILEDATA };
+    res.render('uploadDocument', windowAppendData);
+  } catch (error) {
+    delete error?.config?.['headers'];
+    const Logmessage = {
+      Person_id: TokenDecoder.decoder(SESSION_ID),
+      error_location: `${req.headers.host}${req.originalUrl}`,
+      sessionId: 'null',
+      error_reason: 'File uploading Causes Problem in RFI  - Tenders Api throws error',
+      exception: error,
+    };
+    const Log = new LogMessageFormatter(
+      Logmessage.Person_id,
+      Logmessage.error_location,
+      Logmessage.sessionId,
+      Logmessage.error_reason,
+      Logmessage.exception,
+    );
+    LoggTracer.errorTracer(Log, res);
+  }
 };
 
 /**
@@ -26,20 +81,33 @@ export const GET_UPLOAD_DOC: express.Handler = (req: express.Request, res: expre
  * @POSTController
  */
 
-export const POST_UPLOAD_DOC: express.Handler = (req: express.Request, res: express.Response) => {
+export const POST_UPLOAD_DOC: express.Handler = async (req: express.Request, res: express.Response) => {
   const { rfi_file_started } = req.body;
+  const { SESSION_ID } = req.cookies;
+  const ProjectId = req.session['projectId'];
+  const EventId = req.session['eventId'];
+  const FILE_PUBLISHER_BASEURL = `/tenders/projects/${ProjectId}/events/${EventId}/documents`;
+
   if (rfi_file_started) {
     const { rfi_offline_document } = req.files;
     const multipleFileCheck = Array.isArray(rfi_offline_document);
-
     if (multipleFileCheck) {
       for (const file of rfi_offline_document) {
         tempArray.push(file);
       }
       res.redirect('/rfi/upload-doc');
     } else {
-      tempArray.push(rfi_offline_document);
-      res.redirect('/rfi/upload-doc');
+      const formData = new FormData();
+      formData.append('data', rfi_offline_document.data);
+      formData.append('description', rfi_offline_document.name);
+      formData.append('audience', 'buyer');
+      console.log(formData);
+      const PUBLISH_TO_SERVER = await DynamicFrameworkInstance.file_Instance(SESSION_ID).put(
+        FILE_PUBLISHER_BASEURL,
+        formData,
+      );
+      console.log(PUBLISH_TO_SERVER.data);
+      // res.redirect('/rfi/upload-doc')
     }
   } else res.render('error/500');
 };
