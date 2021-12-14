e152c94e6083fb2514c3c8c1d6e66b23e1151761
Abdul Daim <89971948+daimdev@users.noreply.github.com>
2021-12-14 12:54:06 +0000

    update mimeTypes

--- src/main/features/rfi/util/file/mimetype.ts
+++ src/main/features/rfi/util/file/mimetype.ts
@@ -1,20 +1,22 @@
 
 export const FileMimeType = {
-    "csv": "text/csv",
-    "doc": "application/msword",
-    "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    "jpg": "image/jpeg",
-    "ods": "application/vnd.oasis.opendocument.spreadsheet",
-    "odt": "application/vnd.oasis.opendocument.text",
-    "pdf": "application/pdf",
-    "png": "image/png",
-    "ppt": "application/vnd.ms-powerpoint",
-    "pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-    "rtf": "application/rtf",
-    "txt": "text/plain",
-    "xls": "application/vnd.ms-excel",
-    "xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
-    "xml": "application/xml", 
-    "zip": "application/zip"
+        "csv": "text/csv",
+        "doc": "application/msword",
+        "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "jpg": "image/jpeg",
+        "kml": "application/vnd.google-earth.kml+xml",
+        "ods": "application/vnd.oasis.opendocument.spreadsheet",
+        "odt": "application/vnd.oasis.opendocument.text",
+        "pdf": "application/pdf",
+        "png": "image/png",
+        "ppt": "application/vnd.ms-powerpoint",
+        "pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+        "rdf": "application/rdf+xml", 
+        "rtf": "application/rtf",
+        "txt": "text/plain",
+        "xls": "application/vnd.ms-excel",
+        "xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
+        "xml": "application/xml", 
+        "zip": "application/zip"
 }
 
