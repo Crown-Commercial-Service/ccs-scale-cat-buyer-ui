bb7b1b3cdd6b0831f9714489670dfa749c5848f9
Viswanathan <viswanathan.annamalai@cognizant.com>
2021-12-17 08:29:13 +0000

    Validation changes

--- src/main/public/assets/scripts/validations/rfi_responsedate.js
+++ src/main/public/assets/scripts/validations/rfi_responsedate.js
@@ -3,19 +3,24 @@ const totalElementSelectors = Array.from(Array(5+1).keys()).slice(1);
 
 for(const selector of totalElementSelectors){
 
-    let elementID = "#clarification_date_expanded_"+selector;
+    let elementID = "#rfi_clarification_date_expanded_"+selector;    
     let elementSelector = $(elementID);
+    console.log(elementSelector.length)
+    if(elementSelector.length === 0)
+        elementSelector = $("#eoi_clarification_date_expanded_"+selector); 
     elementSelector.fadeOut();
 }
 
 
 for(const selector of totalElementSelectors){
     let elementID = "#change_clarification_date_"+selector;
-    let elementSelector = $(elementID);
+    let elementSelector = $(elementID);    
     elementSelector.on('click', ()=> {
 
-        let ClickedID = "#clarification_date_expanded_"+selector;
+        let ClickedID = "#rfi_clarification_date_expanded_"+selector;
         let elementSelectorClicked = $(ClickedID);
+        if(elementSelectorClicked.length === 0)
+            elementSelectorClicked = $("#eoi_clarification_date_expanded_"+selector);
         elementSelectorClicked.fadeIn();
     })
 }
@@ -27,8 +32,10 @@ for(const selector of totalElementSelectors){
     let elementSelector = $(elementID);
     elementSelector.on('click', ()=> {
 
-        let ClickedID = "#clarification_date_expanded_"+selector;
+        let ClickedID = "#rfi_clarification_date_expanded_"+selector;
         let elementSelectorClicked = $(ClickedID);
+        if(elementSelectorClicked.length === 0)
+            elementSelectorClicked = $("#eoi_clarification_date_expanded_"+selector);
         elementSelectorClicked.fadeOut();
 
     })
