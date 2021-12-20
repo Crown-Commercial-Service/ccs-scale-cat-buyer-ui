1814979eadba3f83c27fbad86493d5aed0b1fc3d
Viswanathan <viswanathan.annamalai@cognizant.com>
2021-12-20 22:49:53 +0000

    Validation Updated

--- src/main/public/assets/scripts/validations/validate_eoi_budget.js
+++ src/main/public/assets/scripts/validations/validate_eoi_budget.js
@@ -1,18 +1,23 @@
 const ccsZvalidateEoiBudget = (event) => {
   event.preventDefault();
 
-  let fieldCheck = "",
+  let fieldCheck = "", isError = false,
     errorStore = [];
+ccsZremoveErrorMessage($("#eoi_minimum_budget"));
+ccsZremoveErrorMessage($("#eoi_maximum_budget"));
 
-  if (document.getElementById("eoi_minimum_budget").value.trim().length > 0) {
-    fieldCheck = ccsZvalidateWithRegex( "eoi_minimum_budget", "Enter a numeric minimum value", /^\d{1,}$/ );
-    if (fieldCheck !== true) errorStore.push(fieldCheck);
-  }
-
-  fieldCheck = ccsZvalidateWithRegex( "eoi_maximum_budget", "Enter a numeric maximum value", /^\d{1,}$/ );
+  fieldCheck = ccsZvalidateWithRegex("eoi_maximum_budget", "Enter a numeric maximum value", /^\d{1,}$/ );
   if (fieldCheck !== true) errorStore.push(fieldCheck);
 
+  if ($("#eoi_minimum_budget").val().trim().length > 0 && $("#eoi_maximum_budget").val().trim().length > 0) {
+    fieldCheck = ccsZvalidateWithRegex( "eoi_minimum_budget", "Enter a numeric minimum value", /^\d{1,}$/ );
+    if (fieldCheck !== true) errorStore.push(fieldCheck);
+    else if(Number($("#eoi_maximum_budget").val()) < Number($("#eoi_minimum_budget").val())){
+      isError = true;      
+     ccsZaddErrorMessage(document.getElementById("eoi_minimum_budget"), "Minimum budget should be less than maximum budget");
+    }
+  }
 
-  if (errorStore.length === 0) document.forms["eoi_budget_form"].submit();
-  else ccsZPresentErrorSummary(errorStore);
+  if (errorStore.length === 0 && isError == false) document.forms["eoi_budget_form"].submit();
+  ccsZPresentErrorSummary(errorStore);
 };
