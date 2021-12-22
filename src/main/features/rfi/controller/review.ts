f34893a10c2e4bc30c72d785777616eebbceb803
Prathap Mathiyalagan <prathap.mathiyalagan@cognizant.com>
2021-12-22 17:38:53 +0000

    Updated journey service code in review page

--- src/main/features/rfi/controller/review.ts
+++ src/main/features/rfi/controller/review.ts
@@ -1,15 +1,15 @@
 //@ts-nocheck
 import * as express from 'express';
 import * as cmsData from '../../../resources/content/RFI/review.json';
-import {DynamicFrameworkInstance} from '../util/fetch/dyanmicframeworkInstance'
+import { DynamicFrameworkInstance } from '../util/fetch/dyanmicframeworkInstance'
 import { LoggTracer } from '../../../common/logtracer/tracer';
 import { TokenDecoder } from '../../../common/tokendecoder/tokendecoder';
 import { LogMessageFormatter } from '../../../common/logtracer/logmessageformatter';
-import {TenderApi} from '../../../common/util/fetch/procurementService/TenderApiInstance'
+import { TenderApi } from '../../../common/util/fetch/procurementService/TenderApiInstance'
 
 //@GET /rfi/review
-export const GET_RFI_REVIEW  = async (req: express.Request, res: express.Response) => {
-    const {SESSION_ID} = req.cookies;
+export const GET_RFI_REVIEW = async (req: express.Request, res: express.Response) => {
+    const { SESSION_ID } = req.cookies;
     const ProjectID = req.session['projectId'];
     const EventID = req.session['eventId'];
     const BaseURL = `/tenders/projects/${ProjectID}/events/${EventID}`
@@ -19,32 +19,33 @@ export const GET_RFI_REVIEW  = async (req: express.Request, res: express.Respons
         //Buyer Questions
         const BuyerQuestions = ReviewData.nonOCDS.buyerQuestions;
         const BuyerAnsweredAnswers = BuyerQuestions.map(buyer => {
-            const data = buyer.requirementGroups.map(group=> {
+            const data = buyer.requirementGroups.map(group => {
                 const OCDS = group.OCDS;
                 const nonOCDS = group.nonOCDS;
                 nonOCDS.criterian = buyer.id;
-                return {"nonOCDS": nonOCDS, "OCDS": OCDS}
+                return { "nonOCDS": nonOCDS, "OCDS": OCDS }
             });
-            return {"requirement": data}}
+            return { "requirement": data }
+        }
         ).flat();
 
         //JSONData; 
         let Rfi_answered_questions = BuyerAnsweredAnswers.map(rfi => rfi.requirement).flat()
 
-        
-        
+
+
         const GROUP1_Toggle = Rfi_answered_questions.filter(question => question.OCDS.id === 'Group 1')[0];
         const ToggledTrue = GROUP1_Toggle.OCDS.requirements.filter(reqs => reqs.OCDS.id === 'Question 1')[0];
         const selectedToggled = ToggledTrue.nonOCDS.options.map(op => {
-            return {"value": op.value, selected: true}
+            return { "value": op.value, selected: true }
         })
         ToggledTrue.nonOCDS.options = selectedToggled;
         GROUP1_Toggle.OCDS.requirements.map(group => {
-            if(group.OCDS.id === "Question 1") return ToggledTrue
+            if (group.OCDS.id === "Question 1") return ToggledTrue
             else return group
         })
         Rfi_answered_questions = Rfi_answered_questions.map(question => {
-            if(question.OCDS.id === "Group 1") return GROUP1_Toggle
+            if (question.OCDS.id === "Group 1") return GROUP1_Toggle
             else return question
         })
 
@@ -53,7 +54,7 @@ export const GET_RFI_REVIEW  = async (req: express.Request, res: express.Respons
                 "title": question.OCDS.description,
                 "id": question.OCDS.id,
                 "answers": question.OCDS.requirements.map(o => {
-                    return {"question": o.OCDS?.title, "values": o.nonOCDS.options}
+                    return { "question": o.OCDS?.title, "values": o.nonOCDS.options }
                 })
             }
         })
@@ -95,7 +96,7 @@ export const GET_RFI_REVIEW  = async (req: express.Request, res: express.Respons
         const procurementColleagues = isNotProcurementLeadData.map(colleague => colleague.OCDS.contact);
 
 
-        
+
         /**
          * @UploadedDocuments
          */
@@ -106,7 +107,7 @@ export const GET_RFI_REVIEW  = async (req: express.Request, res: express.Respons
         const FetchDocuments = await DynamicFrameworkInstance.Instance(SESSION_ID).get(FILE_PUBLISHER_BASEURL);
         const FETCH_FILEDATA = FetchDocuments.data;
 
-        const FileNameStorage = FETCH_FILEDATA.map(file=> file.fileName)
+        const FileNameStorage = FETCH_FILEDATA.map(file => file.fileName)
 
 
         const agreement_id = req.session['agreement_id'];
@@ -114,26 +115,26 @@ export const GET_RFI_REVIEW  = async (req: express.Request, res: express.Respons
         const event_id = req.session['eventId'];
 
         const GROUPINCLUDING_CRITERIANID = Rfi_answered_questions.filter(data => data.OCDS.id !== "Key Dates").map(data => {
-         return {
-             "criterian":  data.nonOCDS.criterian,
-             "id": data.OCDS.id
+            return {
+                "criterian": data.nonOCDS.criterian,
+                "id": data.OCDS.id
             }
         })
 
         const RFI_ANSWER_STORAGE = [];
 
-        for(const dataOFRFI of RFI_DATA_WITHOUT_KEYDATES){
-            for(const dataOFCRITERIAN of GROUPINCLUDING_CRITERIANID){
-                if(dataOFRFI.id === dataOFCRITERIAN.id){
-                    const formattedData = {...dataOFRFI, "criterian": dataOFCRITERIAN.criterian}
-                   RFI_ANSWER_STORAGE.push(formattedData)
+        for (const dataOFRFI of RFI_DATA_WITHOUT_KEYDATES) {
+            for (const dataOFCRITERIAN of GROUPINCLUDING_CRITERIANID) {
+                if (dataOFRFI.id === dataOFCRITERIAN.id) {
+                    const formattedData = { ...dataOFRFI, "criterian": dataOFCRITERIAN.criterian }
+                    RFI_ANSWER_STORAGE.push(formattedData)
                 }
             }
         }
-        
+
         const appendData = {
-            rfi_data : RFI_ANSWER_STORAGE,
-            rfi_keydates : RFI_DATA_TIMELINE_DATES[0],
+            rfi_data: RFI_ANSWER_STORAGE,
+            rfi_keydates: RFI_DATA_TIMELINE_DATES[0],
             data: cmsData,
             project_name: project_name,
             procurementLead,
@@ -150,22 +151,22 @@ export const GET_RFI_REVIEW  = async (req: express.Request, res: express.Respons
         console.log('Something went wrong, please review the logit error log for more information');
         delete error?.config?.['headers'];
         const Logmessage = {
-          Person_id: TokenDecoder.decoder(SESSION_ID),
-          error_location: `${req.headers.host}${req.originalUrl}`,
-          sessionId: 'null',
-          error_reason: 'Dyanamic framework throws error - Tender Api is causing problem',
-          exception: error,
+            Person_id: TokenDecoder.decoder(SESSION_ID),
+            error_location: `${req.headers.host}${req.originalUrl}`,
+            sessionId: 'null',
+            error_reason: 'Dyanamic framework throws error - Tender Api is causing problem',
+            exception: error,
         };
         const Log = new LogMessageFormatter(
-          Logmessage.Person_id,
-          Logmessage.error_location,
-          Logmessage.sessionId,
-          Logmessage.error_reason,
-          Logmessage.exception,
+            Logmessage.Person_id,
+            Logmessage.error_location,
+            Logmessage.sessionId,
+            Logmessage.error_reason,
+            Logmessage.exception,
         );
         LoggTracer.errorTracer(Log, res);
     }
-  
+
 
 
 
@@ -177,7 +178,13 @@ export const GET_RFI_REVIEW  = async (req: express.Request, res: express.Respons
 //@POST  /rfi/review
 
 
-export const POST_RFI_REVIEW  = (req: express.Request, res: express.Response) => {
-   
+export const POST_RFI_REVIEW = async (req: express.Request, res: express.Response) => {
+
+    const { SESSION_ID } = req.cookies;
+    const eventId = req.session['eventId'];
+    const response = await TenderApi.Instance(SESSION_ID).put(`journeys/${eventId}/steps/2`, 'Completed');
+    if (response.status == HttpStatusCode.OK) {
+        await TenderApi.Instance(SESSION_ID).put(`journeys/${eventId}/steps/15`, 'Completed');
+    }
     res.redirect('/rfi/event-sent')
 }
