97642e9310a91816b9f989b108df52368b273c08
Abdul Daim <89971948+daimdev@users.noreply.github.com>
2021-12-22 13:49:27 +0000

    Feature/RFI -Publish full details

--- src/main/features/rfi/controller/review.ts
+++ src/main/features/rfi/controller/review.ts
@@ -5,7 +5,7 @@ import {DynamicFrameworkInstance} from '../util/fetch/dyanmicframeworkInstance'
 import { LoggTracer } from '../../../common/logtracer/tracer';
 import { TokenDecoder } from '../../../common/tokendecoder/tokendecoder';
 import { LogMessageFormatter } from '../../../common/logtracer/logmessageformatter';
-
+import {TenderApi} from '../../../common/util/fetch/procurementService/TenderApiInstance'
 
 //@GET /rfi/review
 export const GET_RFI_REVIEW  = async (req: express.Request, res: express.Response) => {
@@ -30,6 +30,8 @@ export const GET_RFI_REVIEW  = async (req: express.Request, res: express.Respons
 
         //JSONData; 
         let Rfi_answered_questions = BuyerAnsweredAnswers.map(rfi => rfi.requirement).flat()
+
+        
         
         const GROUP1_Toggle = Rfi_answered_questions.filter(question => question.OCDS.id === 'Group 1')[0];
         const ToggledTrue = GROUP1_Toggle.OCDS.requirements.filter(reqs => reqs.OCDS.id === 'Question 1')[0];
@@ -69,14 +71,77 @@ export const GET_RFI_REVIEW  = async (req: express.Request, res: express.Respons
             }
         })
 
+
+
         const RFI_DATA_WITHOUT_KEYDATES = FilteredSetWithTrue.filter(obj => obj.id !== "Key Dates");
-
         const RFI_DATA_TIMELINE_DATES = FilteredSetWithTrue.filter(obj => obj.id === 'Key Dates')
+        const project_name = req.session.project_name;
 
+        const projectId = req.session.projectId;
+        /**
+         * @ProcurementLead
+         */
+        const procurementLeadURL = `/tenders/projects/${projectId}/users`;
+        const procurementUserData = await TenderApi.Instance(SESSION_ID).get(procurementLeadURL);
+        const ProcurementUsers = procurementUserData.data;
+        const procurementLead = ProcurementUsers.filter(user => user.nonOCDS.projectOwner)[0].OCDS.contact;
+
+
+        /**
+         * @ProcurementCollegues
+         */
+
+        const isNotProcurementLeadData = ProcurementUsers.filter(user => !user.nonOCDS.projectOwner);
+        const procurementColleagues = isNotProcurementLeadData.map(colleague => colleague.OCDS.contact);
+
+
+        
+        /**
+         * @UploadedDocuments
+         */
+
+        const EventId = req.session['eventId'];
+
+        const FILE_PUBLISHER_BASEURL = `/tenders/projects/${projectId}/events/${EventId}/documents`
+        const FetchDocuments = await DynamicFrameworkInstance.Instance(SESSION_ID).get(FILE_PUBLISHER_BASEURL);
+        const FETCH_FILEDATA = FetchDocuments.data;
+
+        const FileNameStorage = FETCH_FILEDATA.map(file=> file.fileName)
+
+
+        const agreement_id = req.session['agreement_id'];
+        const proc_id = req.session['projectId'];
+        const event_id = req.session['eventId'];
+
+        const GROUPINCLUDING_CRITERIANID = Rfi_answered_questions.filter(data => data.OCDS.id !== "Key Dates").map(data => {
+         return {
+             "criterian":  data.nonOCDS.criterian,
+             "id": data.OCDS.id
+            }
+        })
+
+        const RFI_ANSWER_STORAGE = [];
+
+        for(const dataOFRFI of RFI_DATA_WITHOUT_KEYDATES){
+            for(const dataOFCRITERIAN of GROUPINCLUDING_CRITERIANID){
+                if(dataOFRFI.id === dataOFCRITERIAN.id){
+                    const formattedData = {...dataOFRFI, "criterian": dataOFCRITERIAN.criterian}
+                   RFI_ANSWER_STORAGE.push(formattedData)
+                }
+            }
+        }
+        
         const appendData = {
-            rfi_data : RFI_DATA_WITHOUT_KEYDATES,
+            rfi_data : RFI_ANSWER_STORAGE,
             rfi_keydates : RFI_DATA_TIMELINE_DATES[0],
-            data: cmsData
+            data: cmsData,
+            project_name: project_name,
+            procurementLead,
+            procurementColleagues,
+            documents: FileNameStorage,
+            agreement_id,
+            proc_id,
+            event_id
         }
 
         res.render('review', appendData)
