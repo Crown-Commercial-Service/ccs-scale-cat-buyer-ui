0b78bb7a27843cf288be62f9629f5b2ffe6f4922
Viswanathan <viswanathan.annamalai@cognizant.com>
2021-12-23 12:06:39 +0000

    Bug fix for budget and review screen

--- src/main/public/assets/scripts/validations/validate_eoi_budget.js
+++ src/main/public/assets/scripts/validations/validate_eoi_budget.js
@@ -13,11 +13,11 @@ ccsZremoveErrorMessage($("#eoi_maximum_budget"));
     fieldCheck = ccsZvalidateWithRegex( "eoi_minimum_budget", "Enter a numeric minimum value", /^\d{1,}$/ );
     if (fieldCheck !== true) errorStore.push(fieldCheck);
     else if(Number($("#eoi_maximum_budget").val()) < Number($("#eoi_minimum_budget").val())){
-      isError = true;      
-     ccsZaddErrorMessage(document.getElementById("eoi_minimum_budget"), "Minimum budget should be less than maximum budget");
+      errorStore.push(ccsZvalidateWithRegex("eoi_minimum_budget", "Minimum budget should be less than maximum budget",/(?=a)b/));    
     }
   }
 
-  if (errorStore.length === 0 && isError == false) document.forms["eoi_budget_form"].submit();
+  if (errorStore.length === 0) document.forms["eoi_budget_form"].submit();
+  else
   ccsZPresentErrorSummary(errorStore);
 };
