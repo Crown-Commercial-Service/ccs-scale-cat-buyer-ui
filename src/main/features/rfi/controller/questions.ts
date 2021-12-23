f7eb6758b6639703fdf5c83ab819b7fae34daba2
ulysses <ulysses.alvarez@cognizant.com>
2021-12-23 10:57:27 +0000

    error msg for vetting in eoi and rfi.

--- src/main/features/rfi/controller/questions.ts
+++ src/main/features/rfi/controller/questions.ts
@@ -433,7 +433,7 @@ const findErrorText = (data: any) => {
     else if (requirement.nonOCDS.questionType == 'Value' && requirement.nonOCDS.multiAnswer === false)
       errorText = 'You must provide the organization name';
     else if (requirement.nonOCDS.questionType == 'SingleSelect' && requirement.nonOCDS.multiAnswer === false)
-      errorText = 'You must provide a security clearance level before proceeding';
+      errorText = 'You must xxxxxxx provide a security clearance level before proceeding';
     else if (requirement.nonOCDS.questionType == 'Text' && requirement.nonOCDS.multiAnswer === false)
       errorText = 'You must enter information here';
   });
--- src/main/public/assets/scripts/validations/validate_eoi_security.js
+++ src/main/public/assets/scripts/validations/validate_eoi_security.js
@@ -1,12 +1,11 @@
 const ccsZvalidateEoiSecurity = (event) => {
   event.preventDefault();
 
-  let fieldCheck = "",
-    errorStore = [];
+  // let { fieldCheck, errorStore } = initializeErrorStoreForFieldCheck(event);
 
-  fieldCheck = ccsZisOptionChecked( "ccs_vetting_type", "You must provide a security clearance level before proceeding");
-  if (fieldCheck !== true) errorStore.push(fieldCheck);
+  // fieldCheck = ccsZisOptionChecked( "ccs_vetting_type", "You must provide a security clearance level before proceeding");
+  // if (fieldCheck !== true) errorStore.push(fieldCheck);
 
-  if (errorStore.length === 0) document.forms["ccs_eoi_vetting_form"].submit();
-  else ccsZPresentErrorSummary(errorStore);
+  // if (errorStore.length === 0) document.forms["ccs_eoi_vetting_form"].submit();
+  // else ccsZPresentErrorSummary(errorStore);
 };
