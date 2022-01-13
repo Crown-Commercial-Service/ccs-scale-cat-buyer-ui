feaa30b0d1bf0152afdcaefa888a9ba5ee7fd9a9
Cesar <cesarperezfernandez87@gmail.com>
2022-01-13 13:06:55 +0100

    feature: added subtask to CA steps

--- src/main/features/requirements/controller/ca-taskList.ts
+++ src/main/features/requirements/controller/ca-taskList.ts
@@ -8,38 +8,37 @@ import { LoggTracer } from '../../../common/logtracer/tracer';
 import { statusStepsDataFilter } from '../../../utils/statusStepsDataFilter';
 
 export const GET_CAPABILITY_ASSESSMENT = async (req: express.Request, res: express.Response) => {
-    const { SESSION_ID } = req.cookies;
-  const {
-    lotId,
-    agreementLotName,
-    agreementName,
-    eventId,
-    projectId,
-    agreement_id,
-    releatedContent,
-    project_name,
-  } = req.session;
+  const { SESSION_ID } = req.cookies;
+  const { lotId, agreementLotName, agreementName, eventId, projectId, agreement_id, releatedContent, project_name } =
+    req.session;
   const agreementId_session = agreement_id;
-    const { isJaggaerError } = req.session;
-    req.session['isJaggaerError'] = false;
-    res.locals.agreement_header = { agreementName, project_name, agreementId_session, agreementLotName, lotId, error: isJaggaerError };
-    try {
-      const { data: journeySteps } = await TenderApi.Instance(SESSION_ID).get(`journeys/${eventId}/steps`);
-      statusStepsDataFilter(chooseRouteData, journeySteps, 'AC', agreement_id, projectId, eventId);
-      const windowAppendData = { data: chooseRouteData, lotId, agreementLotName, releatedContent };
-      await TenderApi.Instance(SESSION_ID).put(`journeys/${eventId}/steps/3`, 'In progress');
-      res.render('ca-taskList', windowAppendData);
-    } catch (error) {
-      req.session['isJaggaerError'] = true;
-      LoggTracer.errorLogger(
-        res,
-        error,
-        `${req.headers.host}${req.originalUrl}`,
-        null,
-        TokenDecoder.decoder(SESSION_ID),
-        'Journey service - Put failed - AC task list page',
-        true,
-      );
-    }
+  const { isJaggaerError } = req.session;
+  req.session['isJaggaerError'] = false;
+  res.locals.agreement_header = {
+    agreementName,
+    project_name,
+    agreementId_session,
+    agreementLotName,
+    lotId,
+    error: isJaggaerError,
+  };
+  try {
+    await TenderApi.Instance(SESSION_ID).put(`journeys/${eventId}/steps/71`, 'Optional'); //todo: remove later
+    const { data: journeySteps } = await TenderApi.Instance(SESSION_ID).get(`journeys/${eventId}/steps`);
+    statusStepsDataFilter(chooseRouteData, journeySteps, 'CA', agreement_id, projectId, eventId);
+    const windowAppendData = { data: chooseRouteData, lotId, agreementLotName, releatedContent };
+    //await TenderApi.Instance(SESSION_ID).put(`journeys/${eventId}/steps/3`, 'In progress');
+    res.render('ca-taskList', windowAppendData);
+  } catch (error) {
+    req.session['isJaggaerError'] = true;
+    LoggTracer.errorLogger(
+      res,
+      error,
+      `${req.headers.host}${req.originalUrl}`,
+      null,
+      TokenDecoder.decoder(SESSION_ID),
+      'Journey service - Put failed - AC task list page',
+      true,
+    );
+  }
 };
-
