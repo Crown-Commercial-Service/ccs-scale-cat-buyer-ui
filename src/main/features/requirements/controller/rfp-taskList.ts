3845a5e0cf05d9d9b043cad3b5eadafe2a9f2b9d
ulysses <ulysses.alvarez@cognizant.com>
2022-02-09 12:24:24 +0000

    fix issues with event type for DAA and rfp-tasklist.

--- src/main/features/requirements/controller/rfp-taskList.ts
+++ src/main/features/requirements/controller/rfp-taskList.ts
@@ -25,6 +25,15 @@ export const RFP_REQUIREMENT_TASK_LIST = async (req: express.Request, res: expre
   const { isJaggaerError } = req.session;
   const { assessmentId } = currentEvent;
   req.session['isJaggaerError'] = false;
+  const itemList = [
+    'Data',
+    'Technical',
+    'IT Ops',
+    'Product Delivery',
+    'QAT',
+    'User Centred Design',
+    'No DDaT Cluster Mapping',
+  ];
   res.locals.agreement_header = { agreementName, project_name, agreementId_session, agreementLotName, lotid };
   const appendData = { data: chooseRouteData, releatedContent, error: isJaggaerError };
   try {
@@ -35,78 +44,6 @@ export const RFP_REQUIREMENT_TASK_LIST = async (req: express.Request, res: expre
     await TenderApi.Instance(SESSION_ID).put(`journeys/${projectId}/steps/30`, 'In progress');
     await TenderApi.Instance(SESSION_ID).put(`journeys/${projectId}/steps/31`, 'In progress');
     await TenderApi.Instance(SESSION_ID).put(`journeys/${projectId}/steps/32`, 'In progress');
-
-    const ASSESSTMENT_BASEURL = `/assessments/${assessmentId}`;
-    const ALL_ASSESSTMENTS = await TenderApi.Instance(SESSION_ID).get(ASSESSTMENT_BASEURL);
-    const ALL_ASSESSTMENTS_DATA = ALL_ASSESSTMENTS.data;
-    const EXTERNAL_ID = ALL_ASSESSTMENTS_DATA['external-tool-id'];
-
-    const CAPACITY_BASEURL = `assessments/tools/${EXTERNAL_ID}/dimensions`;
-    const CAPACITY_DATA = await TenderApi.Instance(SESSION_ID).get(CAPACITY_BASEURL);
-    const CAPACITY_DATASET = CAPACITY_DATA.data;
-
-    const AddedWeigtagedtoCapacity = CAPACITY_DATASET.map(acapacity => {
-      const { name, weightingRange, options } = acapacity;
-      const AddedPropsToOptions = options.map(anOpt => {
-        return {
-          ...anOpt,
-          Weightagename: name,
-          Weightage: weightingRange,
-        };
-      });
-      return AddedPropsToOptions;
-    }).flat();
-
-    const UNIQUEFIELDNAME = AddedWeigtagedtoCapacity.map(capacity => {
-      return {
-        designation: capacity.name,
-        ...capacity?.groups?.[0],
-        Weightagename: capacity.Weightagename,
-        Weightage: capacity.Weightage,
-      };
-    });
-
-    const UNIQUEELEMENTS_FIELDNAME = [...new Set(UNIQUEFIELDNAME.map(designation => designation.name))].map(cursor => {
-      const ELEMENT_IN_UNIQUEFIELDNAME = UNIQUEFIELDNAME.filter(item => item.name === cursor);
-      return {
-        'job-category': cursor,
-        data: ELEMENT_IN_UNIQUEFIELDNAME,
-      };
-    });
-
-    const ITEMLIST = UNIQUEELEMENTS_FIELDNAME.map((designation, index) => {
-      const weightage = designation.data?.[0]?.Weightage;
-      return {
-        url: `#section${index + 1}`,
-        text: designation['job-category'],
-        subtext: `${weightage.min}% / ${weightage.max}%`,
-      };
-    });
-
-    const UNIQUEJOBDESIGNATIONS = UNIQUEELEMENTS_FIELDNAME.map(designation => {
-      const jobCategory = designation['job-category'];
-      const { data } = designation;
-      const uniqueElements = [...new Set(data.map(designation => designation.designation))];
-      return uniqueElements;
-    }).flat();
-
-    const UNIQUE_JOB_IDENTIFIER = UNIQUEELEMENTS_FIELDNAME.map(element => {
-      const { data } = element;
-      const JobCategory = element['job-category'];
-      let JOBSTORAGE = [];
-      for (const JOB of UNIQUEJOBDESIGNATIONS) {
-        const ElementFinder = data.filter(data => data.designation === JOB)[0];
-        JOBSTORAGE.push(ElementFinder);
-      }
-      JOBSTORAGE = JOBSTORAGE.filter(items => items != null);
-      return {
-        'job-category': JobCategory,
-        data: JOBSTORAGE,
-      };
-    });
-    req.session.designations.push(...UNIQUE_JOB_IDENTIFIER);
-    req.session.tableItems.push(...ITEMLIST);
-    req.session.dimensions.push(...CAPACITY_DATASET);
 
     res.render('rfp-taskList', appendData);
   } catch (error) {
