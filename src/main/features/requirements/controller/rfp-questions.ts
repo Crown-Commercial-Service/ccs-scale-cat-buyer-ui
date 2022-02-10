6859d1f3be89428147658e2533b33a653a1cf7a6
Abdul Daim <89971948+daimdev@users.noreply.github.com>
2022-02-10 15:01:27 +0000

    feature/ SCAT-3289 - resource and vetting

--- src/main/features/requirements/controller/rfp-questions.ts
+++ src/main/features/requirements/controller/rfp-questions.ts
@@ -126,7 +126,7 @@ export const RFP_GET_QUESTIONS = async (req: express.Request, res: express.Respo
     } 
     const POSITIONEDELEMENTS = [...new Set(TemporaryObjStorage.map(JSON.stringify))].map(JSON.parse).filter(item => !item.nonOCDS.dependant)
 
-    console.log(POSITIONEDELEMENTS)
+    console.log(fetch_dynamic_api_data)
    // res.json(POSITIONEDELEMENTS)
     const { isFieldError } = req.session;
     const data = {
@@ -184,6 +184,7 @@ export const RFP_GET_QUESTIONS = async (req: express.Request, res: express.Respo
  */
 // path = '/rfp/questionnaire'
 export const RFP_POST_QUESTION = async (req: express.Request, res: express.Response) => {
+  console.log(req.body)
   try {
     const { proc_id, event_id, id, group_id, stop_page_navigate } = req.query;
     const agreement_id = req.session.agreement_id;
