53baed9130476fe7df11bc341eef1244b1aa44f9
Prathap Mathiyalagan <prathap.mathiyalagan@cognizant.com>
2022-02-15 16:23:26 +0000

    Removed the running event API

--- src/main/features/dashboard/path.ts
+++ src/main/features/dashboard/path.ts
@@ -1,4 +1,4 @@
-import { AUTH } from '../../common/middlewares/oauthservice/authstatecheck';
+/* import { AUTH } from '../../common/middlewares/oauthservice/authstatecheck';
 import {DASHBOARD_CONTROLLER} from './controller/index';
 import {DASHBOARD_PATHS} from './model/dashboardConstants';
 import { Application } from 'express';
@@ -7,4 +7,14 @@ import {EventEngagementMiddleware} from '../../common/middlewares/event-manageme
 export default function(app: Application): void {
  // This is the reciever callback after getting the token
  app.get(DASHBOARD_PATHS.DASHBOARD, [AUTH, EventEngagementMiddleware.GetEvents], DASHBOARD_CONTROLLER.DASHBOARD);
+} */
+
+import { AUTH } from '../../common/middlewares/oauthservice/authstatecheck';
+import {DASHBOARD_CONTROLLER} from './controller/index';
+import {DASHBOARD_PATHS} from './model/dashboardConstants';
+import { Application } from 'express';
+
+export default function(app: Application): void {
+ // This is the reciever callback after getting the token
+ app.get(DASHBOARD_PATHS.DASHBOARD, AUTH, DASHBOARD_CONTROLLER.DASHBOARD);
 }
