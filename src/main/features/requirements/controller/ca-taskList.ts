08d6a3b39e4ec5b9f9379572c5fc9e7ff91fd275
Abdul Daim <89971948+daimdev@users.noreply.github.com>
2022-02-22 16:41:59 +0000

    feature/SCAT 3289 - Set your resources and vetting requirement

--- src/main/features/requirements/controller/ca-taskList.ts
+++ src/main/features/requirements/controller/ca-taskList.ts
@@ -82,6 +82,7 @@ export const CA_REQUIREMENT_TASK_LIST = async (req: express.Request, res: expres
     const CAPACITY_BASEURL = `assessments/tools/${EXTERNAL_ID}/dimensions`;
     const CAPACITY_DATA = await TenderApi.Instance(SESSION_ID).get(CAPACITY_BASEURL);
     const CAPACITY_DATASET = CAPACITY_DATA.data;
+    
 
     const AddedWeigtagedtoCapacity = CAPACITY_DATASET.map(acapacity => {
       const { name, weightingRange, options } = acapacity;
@@ -148,7 +149,8 @@ export const CA_REQUIREMENT_TASK_LIST = async (req: express.Request, res: expres
     req.session.tableItems = [...ITEMLIST];
     req.session.dimensions = [...CAPACITY_DATASET];
 
-    res.render('ca-taskList', windowAppendData);
+  //  res.json(CAPACITY_DATASET)
+   res.render('ca-taskList', windowAppendData);
   } catch (error) {
     req.session['isJaggaerError'] = true;
     LoggTracer.errorLogger(
