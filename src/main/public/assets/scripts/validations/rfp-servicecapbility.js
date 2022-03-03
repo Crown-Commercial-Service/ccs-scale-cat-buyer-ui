25043ef7b992ab2d9eb4dcafbc2d3261dc173373
Abdul Daim <89971948+daimdev@users.noreply.github.com>
2022-03-03 12:34:57 +0000

    feature/SCAT 1070-Select your service Capabilities

---
+++ src/main/public/assets/scripts/validations/rfp-servicecapbility.js
@@ -0,0 +1,24 @@
+
+const totalElement = $('.ons-list__link').length;
+
+var arrayOfHeadings = [];
+for(var a =0; a < totalElement; a++){
+
+    arrayOfHeadings.push(document.getElementsByClassName('ons-list__link')[a].innerHTML.split('<div ')[0].split(' ').join('_'))
+
+}
+
+
+for(var a =0; a < arrayOfHeadings.length; a++){
+
+    const classTarget = arrayOfHeadings[a]+'_t';
+    const classFiller = arrayOfHeadings[a];
+
+    $(`.${classTarget}`).on('click', ()=> {
+        $(`.${classFiller}`).attr('checked', true);
+        $('html,body').animate({
+            scrollTop: $("#scrollTo").offset().top - $(window).height()/2
+         }, 1000);
+    })
+
+}
