709bf726a6f37cb76fcb5608ce3bde340f3d363e
Abdul Daim <89971948+daimdev@users.noreply.github.com>
2022-03-10 14:32:48 +0000

    Added Tender service for posting data

--- src/main/features/requirements/controller/rfp-work-completed.ts
+++ src/main/features/requirements/controller/rfp-work-completed.ts
@@ -21,18 +21,44 @@ export const RFP_GET_WORK_COMPLETED = async (req: express.Request, res: express.
       isError,
       errorText,
       currentEvent,
-      choosenViewPath
+      choosenViewPath,
+      eventId
     } = req.session;
 
+    try {
+        const CriterianId = 'Criterion 3'
+        const GroupId = 'Group 7';
+    
+        
+        const BaseURL = `/tenders/projects/${projectId}/events/${eventId}/criteria/${CriterianId}/groups/${GroupId}/questions`
+        const Response = await TenderApi.Instance(SESSION_ID).get(BaseURL);
+        const ocdData = Response.data[0];
 
-    const windowAppendData = {
-        data: cmsData,
-        releatedContent,
-        choosenViewPath
+        console.log(ocdData)
+    
+    
+        const windowAppendData = {
+            data: cmsData,
+            releatedContent,
+            choosenViewPath,
+            response: ocdData
+        }
+    
+        await TenderApi.Instance(SESSION_ID).put(`journeys/${projectId}/steps/54`, 'In progress');
+        res.render('rfp-work-completed.njk', windowAppendData)
+    } catch (error) {
+        LoggTracer.errorLogger(
+            res,
+            error,
+            `${req.headers.host}${req.originalUrl}`,
+            null,
+            TokenDecoder.decoder(SESSION_ID),
+            'FC- GET - Work completed Error',
+            true,
+        )
     }
 
-
-    res.render('rfp-work-completed.njk', windowAppendData)
+   
 
 }
 
@@ -40,7 +66,48 @@ export const RFP_GET_WORK_COMPLETED = async (req: express.Request, res: express.
 
 
 export const RFP_POST_WORK_COMPLETED = async (req: express.Request, res: express.Response) => {
-    const {completed_text} = req.body;
+
+
+    try {
+        const {completed_text} = req.body;
+        const { SESSION_ID } = req.cookies;
+        const { 
+          projectId,
+          eventId
+        } = req.session;
+        const CriterianId = 'Criterion 3'
+        const GroupId = 'Group 7';
+        const QuestionID = 'Question 8';
+        const BaseURL = `/tenders/projects/${projectId}/events/${eventId}/criteria/${CriterianId}/groups/${GroupId}/questions/${QuestionID}`
+
+        const REQUESTBODY = {
+            "nonOCDS": {
+                "answered": true,
+                "options": [
+                    {
+                        "value": completed_text
+                    }
+                ]
+            }
+        }
+
+        await TenderApi.Instance(SESSION_ID).put(BaseURL, REQUESTBODY);
+        await TenderApi.Instance(SESSION_ID).put(`journeys/${projectId}/steps/54`, 'Completed');
+        res.redirect('/rfp/task-list');
+
+
+    } catch (error) {
+        LoggTracer.errorLogger(
+          res,
+          error,
+          `${req.headers.host}${req.originalUrl}`,
+          null,
+          TokenDecoder.decoder(SESSION_ID),
+          'FC - POST - Work completed Error ',
+          true,
+        );
+      }
+    
 
 
 }
