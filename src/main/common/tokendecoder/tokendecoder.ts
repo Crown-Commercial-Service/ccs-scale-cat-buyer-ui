8ea3075ad92868f26414d4717170de506a88b22b
Prathap Mathiyalagan <prathap.mathiyalagan@cognizant.com>
2022-03-17 16:02:17 +0000

    Revert "jsonwebtoken upgrade"
    This reverts commit c309b1761c4c81681adea610c42bdf542bbb61af.

--- src/main/common/tokendecoder/tokendecoder.ts
+++ src/main/common/tokendecoder/tokendecoder.ts
@@ -7,8 +7,8 @@ import { decode } from 'jsonwebtoken'
 export class TokenDecoder {
 
     static decoder = (access_token: string) => {
-        const information_decoded = decode(access_token, { json: true });
-        const uid = information_decoded?.uid;
+        const information_decoded = decode(access_token, { complete: true });
+        const uid = information_decoded?.payload?.uid;
         return uid;
     }
 }
