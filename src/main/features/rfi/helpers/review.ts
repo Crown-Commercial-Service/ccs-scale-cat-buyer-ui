7c6e5ca325c84611a6279adf1c93e14e30ccc302
Charunya <charunya.lnu@cognizant.com>
2022-04-22 11:21:47 +0530

    Commit to 4146

--- src/main/features/rfi/helpers/review.ts
+++ src/main/features/rfi/helpers/review.ts
@@ -7,6 +7,8 @@ import { TokenDecoder } from '../../../common/tokendecoder/tokendecoder';
 import { LogMessageFormatter } from '../../../common/logtracer/logmessageformatter';
 import { TenderApi } from '../../../common/util/fetch/procurementService/TenderApiInstance';
 import { HttpStatusCode } from '../../../errors/httpStatusCodes';
+import { title } from 'process';
+import { GetLotSuppliers } from '../../shared/supplierService';
 
 export const RFI_REVIEW_HELPER = async (req: express.Request, res: express.Response, viewError: boolean, apiError: boolean) => {
   const { SESSION_ID } = req.cookies;
@@ -36,14 +38,24 @@ export const RFI_REVIEW_HELPER = async (req: express.Request, res: express.Respo
     const selectedToggled = ToggledTrue.nonOCDS.options.map(op => {
       return { value: op.value, selected: true };
     });
+    console.log("GROUP1_Toggle:" +GROUP1_Toggle)
+    console.log("ToggledTrue: " +ToggledTrue)
+    console.log("selectedToggled: "+selectedToggled)
     ToggledTrue.nonOCDS.options = selectedToggled;
     GROUP1_Toggle.OCDS.requirements.map(group => {
       if (group.OCDS.id === 'Question 1') return ToggledTrue;
-      else return group;
+      else {
+        console.log("Group:" +group)
+        return group;
+      }
     });
     Rfi_answered_questions = Rfi_answered_questions.map(question => {
       if (question.OCDS.id === 'Group 1') return GROUP1_Toggle;
-      else return question;
+      
+      else {
+        console.log("Questions:" +question)
+        return question;
+      }
     });
 
     const ExtractedRFI_Answers = Rfi_answered_questions.sort((a: any, b: any) =>
@@ -69,12 +81,15 @@ export const RFI_REVIEW_HELPER = async (req: express.Request, res: express.Respo
           };
         }),
       };
+      console.log(title)
+      console.log(id)
+      console.log(answer)
     });
 
     const RFI_DATA_WITHOUT_KEYDATES = FilteredSetWithTrue.filter(obj => obj.id !== 'Key Dates');
     const RFI_DATA_TIMELINE_DATES = FilteredSetWithTrue.filter(obj => obj.id === 'Key Dates');
     const project_name = req.session.project_name;
-
+console.log(FilteredSetWithTrue)
     const projectId = req.session.projectId;
     /**
      * @ProcurementLead
@@ -124,11 +139,19 @@ export const RFI_REVIEW_HELPER = async (req: express.Request, res: express.Respo
         }
       }
     }
+//Fix for SCAT-4146 - arranging the questions order
+    let expected_rfi_keydates=RFI_DATA_TIMELINE_DATES[0];
+    let temp=expected_rfi_keydates.answer[0].question;
+    expected_rfi_keydates.answer[0].question=expected_rfi_keydates.answer[1].question;
+    expected_rfi_keydates.answer[1].question=temp;
 
+    let supplierList = [];
+    supplierList = await GetLotSuppliers(req);
 
     let appendData = {
       rfi_data: RFI_ANSWER_STORAGE,
-      rfi_keydates: RFI_DATA_TIMELINE_DATES[0],
+      rfi_keydates: expected_rfi_keydates,
+      //rfi_keydates: RFI_DATA_TIMELINE_DATES[0],
       data: cmsData,
       project_name: project_name,
       procurementLead,
@@ -138,12 +161,14 @@ export const RFI_REVIEW_HELPER = async (req: express.Request, res: express.Respo
       proc_id,
       event_id,
       ccs_rfi_type: RFI_ANSWER_STORAGE.length > 0 ? 'all_online' : '',
-      eventStatus: ReviewData.OCDS.status == 'active' ? "published" : null // this needs to be revisited to check the mapping of the planned 
+      eventStatus: ReviewData.OCDS.status == 'active' ? "published" : null, // this needs to be revisited to check the mapping of the planned 
+      suppliers_list:supplierList
     };
 
     if (viewError) {
       appendData = Object.assign({}, { ...appendData, viewError: true, apiError: apiError });
     }
+    console.log(appendData)
     res.render('review', appendData);
   } catch (error) {
     delete error?.config?.['headers'];
