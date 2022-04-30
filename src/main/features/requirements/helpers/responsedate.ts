aeb9316deafaac863c5044eef4e2739b0cd0d703
Dhivya <dhivya.pandiadurai@cognizant.com>
2022-04-30 12:20:08 +0530

    Commit

--- src/main/features/requirements/helpers/responsedate.ts
+++ src/main/features/requirements/helpers/responsedate.ts
@@ -52,44 +52,9 @@ export const RESPONSEDATEHELPER = async (req: express.Request, res: express.Resp
     let rfp_clarification_period;
     let deadline_period;
     let rfp_clarification_endDate;
-    fetchQuestionsData.map(item => {
-      if(item.nonOCDS.options.length > 0) {
-        if(item.nonOCDS.options[0].text === 'Question 1') {
-          selectedDate = item.nonOCDS.options[0].value;
-        }
-        else if(item.nonOCDS.options[0].text === 'Question 2') {
-          rfp_clarification_endDate = item.nonOCDS.options[0].value;
-        }
-        else if(item.nonOCDS.options[0].text === 'Question 3') {
-          rfp_clarification_endDate = item.nonOCDS.options[0].value;
-        }
-        else if(item.nonOCDS.options[0].text === 'Question 4') {
-          rfp_clarification_endDate = item.nonOCDS.options[0].value;
-        }
-        else if(item.nonOCDS.options[0].text === 'Question 5') {
-          rfp_clarification_endDate = item.nonOCDS.options[0].value;
-        }
-        else if(item.nonOCDS.options[0].text === 'Question 6') {
-          rfp_clarification_endDate = item.nonOCDS.options[0].value;
-        }
-        else if(item.nonOCDS.options[0].text === 'Question 7') {
-          rfp_clarification_endDate = item.nonOCDS.options[0].value;
-        }
-        else if(item.nonOCDS.options[0].text === 'Question 8') {
-          rfp_clarification_endDate = item.nonOCDS.options[0].value;
-        }
-        else if(item.nonOCDS.options[0].text === 'Question 9') {
-          rfp_clarification_endDate = item.nonOCDS.options[0].value;
-        }
-        else if(item.nonOCDS.options[0].text === 'Question 10') {
-          rfp_clarification_endDate = item.nonOCDS.options[0].value;
-        }
-        else if(item.nonOCDS.options[0].text === 'Question 11') {
-          rfp_clarification_endDate = item.nonOCDS.options[0].value;
-        }
-      }
-    })
-   
+    let supplier_period_for_clarification_period;
+    let supplier_dealine_for_clarification_period;
+    
     if(req.session.UIDate==null){
       const rfp_clarification_date = moment(new Date(), 'DD/MM/YYYY').format('DD MMMM YYYY');
       const clarification_period_end_date = new Date();
@@ -520,9 +485,262 @@ if (errorTriggered) {
 res.render('rfp-responsedate.njk', appendData);
     }
     else if(req.session.questionID=='Question 4'){
-      
+      rfp_clarification =req.session.rfppublishdate;
+      rfp_clarification_date =moment(
+        rfp_clarification,
+        'YYYY-MM-DD, hh:mm a',
+      ).format('DD MMMM YYYY, hh:mm a');
+     rfp_clarification_period=req.session.clarificationend;
+     rfp_clarification_period_end=moment(
+      rfp_clarification_period,
+      'YYYY-MM-DD, hh:mm a',
+    ).format('DD MMMM YYYY, hh:mm a');
+    if(rfp_clarification_period_end=='Invalid date')rfp_clarification_period_end=rfp_clarification_period;
+     else 
+      rfp_clarification_period_end=rfp_clarification_period_end;
+      let deadline_period_for_clarification =req.session.deadlinepublishresponse;
+     let deadline_period_for_clarification_period=moment(
+      deadline_period_for_clarification,
+      'YYYY-MM-DD, hh:mm a',
+    ).format('DD MMMM YYYY, hh:mm a');
+    if(deadline_period_for_clarification_period=='Invalid date')deadline_period_for_clarification_period=deadline_period_for_clarification;
+     else 
+     deadline_period_for_clarification_period=deadline_period_for_clarification_period;
+    
+    supplier_period_for_clarification_period=req.session.UIDate;
+    let SupplierPeriodDeadLine = supplier_period_for_clarification_period;
+const supplier_dealine_for_clarification_period = moment(SupplierPeriodDeadLine, 'DD MMMM YYYY, hh:mm a').businessAdd(
+  predefinedDays.supplier_deadline,
+)._d;
+supplier_dealine_for_clarification_period.setHours(predefinedDays.defaultEndingHour);
+supplier_dealine_for_clarification_period.setMinutes(predefinedDays.defaultEndingMinutes);
+//6
+const SupplierSubmissionDeadLine = supplier_dealine_for_clarification_period;
+const deadline_for_submission_of_stage_one = moment(SupplierSubmissionDeadLine, 'DD-MM-YYYY').businessAdd(
+  predefinedDays.supplier_period_extra,
+)._d;
+deadline_for_submission_of_stage_one.setHours(predefinedDays.defaultEndingHour);
+deadline_for_submission_of_stage_one.setMinutes(predefinedDays.defaultEndingMinutes);
+//7
+const EvaluationProcessStart = deadline_for_submission_of_stage_one;
+const evaluation_process_start_date = moment(EvaluationProcessStart, 'DD-MM-YYYY').businessAdd(
+  predefinedDays.supplier_period_extra,
+)._d;
+evaluation_process_start_date.setHours(predefinedDays.defaultEndingHour);
+evaluation_process_start_date.setMinutes(predefinedDays.defaultEndingMinutes);
+//8
+const BidderPresentations = evaluation_process_start_date;
+const bidder_presentations_date = moment(BidderPresentations, 'DD-MM-YYYY').businessAdd(
+  predefinedDays.supplier_period_extra,
+)._d;
+bidder_presentations_date.setHours(predefinedDays.defaultEndingHour);
+bidder_presentations_date.setMinutes(predefinedDays.defaultEndingMinutes);
+//9
+const StandstillPeriodStarts = bidder_presentations_date;
+const standstill_period_starts_date = moment(StandstillPeriodStarts, 'DD-MM-YYYY').businessAdd(
+  predefinedDays.supplier_deadline_extra,
+)._d;
+standstill_period_starts_date.setHours(predefinedDays.defaultEndingHour);
+standstill_period_starts_date.setMinutes(predefinedDays.defaultEndingMinutes);
+//10
+const ProposedAward = standstill_period_starts_date;
+const proposed_award_date = moment(ProposedAward, 'DD-MM-YYYY').businessAdd(
+  predefinedDays.supplier_deadline_extra,
+)._d;
+proposed_award_date.setHours(predefinedDays.defaultEndingHour);
+proposed_award_date.setMinutes(predefinedDays.defaultEndingMinutes);
+//11
+const ExpectedSignature = proposed_award_date;
+const expected_signature_date = moment(ExpectedSignature, 'DD-MM-YYYY').businessAdd(
+  predefinedDays.supplier_period_extra,
+)._d;
+expected_signature_date.setHours(predefinedDays.defaultEndingHour);
+expected_signature_date.setMinutes(predefinedDays.defaultEndingMinutes);
+fetchQuestionsData = fetchQuestionsData.sort((current_element, next_element) => {
+  const currentElementID = Number(current_element.OCDS.id.split('Question ').join(''));
+  const nextElementID = Number(next_element.OCDS.id.split('Question ').join(''));
+  return currentElementID - nextElementID;
+});
+const agreementName = req.session.agreementName;
+const lotid = req.session?.lotId;
+const agreementId_session = req.session.agreement_id;
+const agreementLotName = req.session.agreementLotName;
+const project_name = req.session.project_name;
+res.locals.agreement_header = { agreementName, project_name, agreementId_session, agreementLotName, lotid };
+let appendData = {
+  data: cmsData,
+  prompt: prompt,
+  framework: fetchQuestionsData,
+  rfp_clarification_date,
+  rfp_clarification_period_end,
+  deadline_period_for_clarification_period,
+  supplier_period_for_clarification_period,
+  supplier_dealine_for_clarification_period: moment(
+    supplier_dealine_for_clarification_period,
+    'DD-MM-YYYY, hh:mm a',
+  ).format('DD MMMM YYYY, hh:mm a'),
+  deadline_for_submission_of_stage_one: moment(deadline_for_submission_of_stage_one, 'DD-MMYYYY, hh:mm a').format(
+    'DD MMMM YYYY, hh:mm a',
+  ),
+  evaluation_process_start_date: moment(evaluation_process_start_date, 'DD-MM-YYYY, hh:mm a').format(
+    'DD MMMM YYYY, hh:mm a',
+  ),
+  bidder_presentations_date: moment(bidder_presentations_date, 'DD-MM-YYYY, hh:mm a').format(
+    'DD MMMM YYYY, hh:mm a',
+  ),
+  standstill_period_starts_date: moment(standstill_period_starts_date, 'DD-MM-YYYY, hh:mm a').format(
+    'DD MMMM YYYY, hh:mm a',
+  ),
+  proposed_award_date: moment(proposed_award_date, 'DD-MM-YYYY, hh:mm a').format('DD MMMM YYYY, hh:mm a'),
+  expected_signature_date: moment(expected_signature_date, 'DD-MM-YYYY, hh:mm a').format('DD MMMM YYYY, hh:mm a'),
+  releatedContent: req.session.releatedContent,
+};
+if (errorTriggered) {
+  appendData = { ...appendData, error: true, errorMessage: errorItem };
+} else {
+  req.session.timeline = {};
+  req.session.timeline.publish = new Date();
+  req.session.timeline.clarificationPeriodEnd = rfp_clarification_period_end;
+  req.session.timeline.publishResponsesClarificationQuestions = deadline_period_for_clarification_period;
+  req.session.timeline.supplierSubmitResponse = supplier_period_for_clarification_period;
+  req.session.timeline.confirmNextStepsSuppliers = supplier_dealine_for_clarification_period;
+  //
+  req.session.timeline.deadlineForSubmissionOfStageOne = deadline_for_submission_of_stage_one;
+  req.session.timeline.evaluationProcessStartDate = evaluation_process_start_date;
+  req.session.timeline.bidderPresentationsDate = bidder_presentations_date;
+  req.session.timeline.standstillPeriodStartsDate = standstill_period_starts_date;
+  req.session.timeline.proposedAwardDate = proposed_award_date;
+  req.session.timeline.expectedSignatureDate = expected_signature_date;
+}
+res.render('rfp-responsedate.njk', appendData);
           }
     else if(req.session.questionID=='Question 5'){
+      rfp_clarification =req.session.rfppublishdate;
+      rfp_clarification_date =moment(
+        rfp_clarification,
+        'YYYY-MM-DD, hh:mm a',
+      ).format('DD MMMM YYYY, hh:mm a');
+     rfp_clarification_period=req.session.clarificationend;
+     rfp_clarification_period_end=moment(
+      rfp_clarification_period,
+      'YYYY-MM-DD, hh:mm a',
+    ).format('DD MMMM YYYY, hh:mm a');
+    if(rfp_clarification_period_end=='Invalid date')rfp_clarification_period_end=rfp_clarification_period;
+     else 
+      rfp_clarification_period_end=rfp_clarification_period_end;
+    let deadline_period_for_clarification =req.session.deadlinepublishresponse;
+     let deadline_period_for_clarification_period=moment(
+      deadline_period_for_clarification,
+      'YYYY-MM-DD, hh:mm a',
+    ).format('DD MMMM YYYY, hh:mm a');
+    if(deadline_period_for_clarification_period=='Invalid date')deadline_period_for_clarification_period=deadline_period_for_clarification;
+     else 
+     deadline_period_for_clarification_period=deadline_period_for_clarification_period;
+    
+    let supplier_submit=req.session.supplierresponse;
+    let supplier_period_for_clarification_period=moment(
+      supplier_submit,
+      'YYYY-MM-DD, hh:mm a',
+    ).format('DD MMMM YYYY, hh:mm a');
+    if(supplier_period_for_clarification_period=='Invalid date')supplier_period_for_clarification_period=supplier_submit;
+     else 
+     supplier_period_for_clarification_period=supplier_period_for_clarification_period;
+     supplier_dealine_for_clarification_period=req.session.UIDate;
+     const SupplierSubmissionDeadLine = supplier_dealine_for_clarification_period;
+const deadline_for_submission_of_stage_one = moment(SupplierSubmissionDeadLine, 'DD MMMM YYYY, hh:mm a').businessAdd(
+  predefinedDays.supplier_period_extra,
+)._d;
+deadline_for_submission_of_stage_one.setHours(predefinedDays.defaultEndingHour);
+deadline_for_submission_of_stage_one.setMinutes(predefinedDays.defaultEndingMinutes);
+//7
+const EvaluationProcessStart = deadline_for_submission_of_stage_one;
+const evaluation_process_start_date = moment(EvaluationProcessStart, 'DD-MM-YYYY').businessAdd(
+  predefinedDays.supplier_period_extra,
+)._d;
+evaluation_process_start_date.setHours(predefinedDays.defaultEndingHour);
+evaluation_process_start_date.setMinutes(predefinedDays.defaultEndingMinutes);
+//8
+const BidderPresentations = evaluation_process_start_date;
+const bidder_presentations_date = moment(BidderPresentations, 'DD-MM-YYYY').businessAdd(
+  predefinedDays.supplier_period_extra,
+)._d;
+bidder_presentations_date.setHours(predefinedDays.defaultEndingHour);
+bidder_presentations_date.setMinutes(predefinedDays.defaultEndingMinutes);
+//9
+const StandstillPeriodStarts = bidder_presentations_date;
+const standstill_period_starts_date = moment(StandstillPeriodStarts, 'DD-MM-YYYY').businessAdd(
+  predefinedDays.supplier_deadline_extra,
+)._d;
+standstill_period_starts_date.setHours(predefinedDays.defaultEndingHour);
+standstill_period_starts_date.setMinutes(predefinedDays.defaultEndingMinutes);
+//10
+const ProposedAward = standstill_period_starts_date;
+const proposed_award_date = moment(ProposedAward, 'DD-MM-YYYY').businessAdd(
+  predefinedDays.supplier_deadline_extra,
+)._d;
+proposed_award_date.setHours(predefinedDays.defaultEndingHour);
+proposed_award_date.setMinutes(predefinedDays.defaultEndingMinutes);
+//11
+const ExpectedSignature = proposed_award_date;
+const expected_signature_date = moment(ExpectedSignature, 'DD-MM-YYYY').businessAdd(
+  predefinedDays.supplier_period_extra,
+)._d;
+expected_signature_date.setHours(predefinedDays.defaultEndingHour);
+expected_signature_date.setMinutes(predefinedDays.defaultEndingMinutes);
+fetchQuestionsData = fetchQuestionsData.sort((current_element, next_element) => {
+  const currentElementID = Number(current_element.OCDS.id.split('Question ').join(''));
+  const nextElementID = Number(next_element.OCDS.id.split('Question ').join(''));
+  return currentElementID - nextElementID;
+});
+const agreementName = req.session.agreementName;
+const lotid = req.session?.lotId;
+const agreementId_session = req.session.agreement_id;
+const agreementLotName = req.session.agreementLotName;
+const project_name = req.session.project_name;
+res.locals.agreement_header = { agreementName, project_name, agreementId_session, agreementLotName, lotid };
+let appendData = {
+  data: cmsData,
+  prompt: prompt,
+  framework: fetchQuestionsData,
+  rfp_clarification_date,
+  rfp_clarification_period_end,
+  deadline_period_for_clarification_period,
+  supplier_period_for_clarification_period,
+  supplier_dealine_for_clarification_period,
+  deadline_for_submission_of_stage_one: moment(deadline_for_submission_of_stage_one, 'DD-MMYYYY, hh:mm a').format(
+    'DD MMMM YYYY, hh:mm a',
+  ),
+  evaluation_process_start_date: moment(evaluation_process_start_date, 'DD-MM-YYYY, hh:mm a').format(
+    'DD MMMM YYYY, hh:mm a',
+  ),
+  bidder_presentations_date: moment(bidder_presentations_date, 'DD-MM-YYYY, hh:mm a').format(
+    'DD MMMM YYYY, hh:mm a',
+  ),
+  standstill_period_starts_date: moment(standstill_period_starts_date, 'DD-MM-YYYY, hh:mm a').format(
+    'DD MMMM YYYY, hh:mm a',
+  ),
+  proposed_award_date: moment(proposed_award_date, 'DD-MM-YYYY, hh:mm a').format('DD MMMM YYYY, hh:mm a'),
+  expected_signature_date: moment(expected_signature_date, 'DD-MM-YYYY, hh:mm a').format('DD MMMM YYYY, hh:mm a'),
+  releatedContent: req.session.releatedContent,
+};
+if (errorTriggered) {
+  appendData = { ...appendData, error: true, errorMessage: errorItem };
+} else {
+  req.session.timeline = {};
+  req.session.timeline.publish = new Date();
+  req.session.timeline.clarificationPeriodEnd = rfp_clarification_period_end;
+  req.session.timeline.publishResponsesClarificationQuestions = deadline_period_for_clarification_period;
+  req.session.timeline.supplierSubmitResponse = supplier_period_for_clarification_period;
+  req.session.timeline.confirmNextStepsSuppliers = supplier_dealine_for_clarification_period;
+  //
+  req.session.timeline.deadlineForSubmissionOfStageOne = deadline_for_submission_of_stage_one;
+  req.session.timeline.evaluationProcessStartDate = evaluation_process_start_date;
+  req.session.timeline.bidderPresentationsDate = bidder_presentations_date;
+  req.session.timeline.standstillPeriodStartsDate = standstill_period_starts_date;
+  req.session.timeline.proposedAwardDate = proposed_award_date;
+  req.session.timeline.expectedSignatureDate = expected_signature_date;
+}
+res.render('rfp-responsedate.njk', appendData);
       
     }
     else if(req.session.questionID=='Question 6'){
