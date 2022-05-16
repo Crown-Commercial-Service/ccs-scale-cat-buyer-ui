3923fc4dedd81b2bb05579f5a4d39393fce67d24
Charunya <charunya.lnu@cognizant.com>
2022-05-16 13:44:27 +0530

    Fix for SCAT-5010

--- src/main/features/event-management/controller/eventManagement.ts
+++ src/main/features/event-management/controller/eventManagement.ts
@@ -96,7 +96,30 @@ export const EVENT_MANAGEMENT = async (req: express.Request, res: express.Respon
         }
       });
     }
-    if(status=='In Progress'||status=='IN PROGRESS') {
+
+//Get Q&A Count
+    const baseQandAURL = `/tenders/projects/${req.session.projectId}/events/${req.session.eventId}/q-and-a`;
+        const fetchData = await TenderApi.Instance(SESSION_ID).get(baseQandAURL);
+
+
+    if (status == "Published" || status == "Response period closed" ) {
+      let redirectUrl_: string
+      switch (eventType) {
+       
+        case "RFI":
+          const appendData = { data: eventManagementData, status, projectName, eventId, eventType,QAs: fetchData.data, suppliers: localData, unreadMessage: unreadMessage }
+          res.render('eventManagement', appendData)
+          break
+          case "FC":
+            redirectUrl_="/rfp/rfp-unpublishedeventmanagement"    
+            res.redirect(redirectUrl_)     
+            break
+          default:
+            redirectUrl_ = '/event/management'
+            break
+        }
+       
+  } else {
       let redirectUrl: string
       switch (eventType) {
         case "RFI":
@@ -126,24 +149,8 @@ export const EVENT_MANAGEMENT = async (req: express.Request, res: express.Respon
       }
       res.redirect(redirectUrl)
     }
-    else{
-      let redirectUrl_: string
-      switch (eventType) {
        
-        case "RFI":      
-          const appendData = { data: eventManagementData, status, projectName, eventId, eventType, suppliers: localData, unreadMessage: unreadMessage }
-          res.render('eventManagement', appendData)
-          break
-          case "FC":
-            redirectUrl_="/rfp/rfp-unpublishedeventmanagement"    
-            res.redirect(redirectUrl_)     
-            break
-          default:
-            redirectUrl_ = '/event/management'
-            break
-        }
-       
-  }
+  
 
   } catch (err) {
     LoggTracer.errorLogger(
