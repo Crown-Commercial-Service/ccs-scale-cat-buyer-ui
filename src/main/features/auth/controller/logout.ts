7efa73af650417e99e57356fef1590d2b3341858
Balwinder.Singh <Balwinder.Singh@matthey.com>
2022-05-29 19:13:31 +0530

    SCAT-5040

--- src/main/features/auth/controller/logout.ts
+++ src/main/features/auth/controller/logout.ts
@@ -1,4 +1,5 @@
 import * as express from 'express'
+import { AuthorizationRedirect } from './authRedirect';
 const { Logger } = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('logout');
 
@@ -13,5 +14,9 @@ export const LOGOUT = (req : express.Request, res : express.Response)=> {
   req.session.destroy(function(err) {
     logger.info(err);
   })
-  res.redirect('/');
+  //BALWINDER REDIRECT TO CCS PUBLIC PROCUREMENT GATEWAY PPG LOGIN PAGE
+  let redirectURL: string | any = new AuthorizationRedirect();
+  redirectURL = redirectURL.Redirect_Oauth_URL();
+  res.redirect(redirectURL)
+  //res.redirect('/');
 }
