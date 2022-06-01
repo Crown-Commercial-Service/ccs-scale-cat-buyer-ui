96147dccdaf3b5d1af147551ae8d1201d1fbc325
Balwinder.Singh <Balwinder.Singh@matthey.com>
2022-06-01 22:23:10 +0530

    Bugfix/SCAT-5272/SCAT-5270/SCAT-5136/SCAT-5029

--- src/main/public/assets/scripts/validations/validate_rfp_strategy.js
+++ src/main/public/assets/scripts/validations/validate_rfp_strategy.js
@@ -26,7 +26,6 @@ const ccsZvalidateRfPStrategy = event => {
   let fieldCheck = '';
   errorStore.length = 0;
   const pageHeading = document.getElementById('page-heading').innerHTML;
-
   if ($('#ccs_vetting_type') !== undefined) {
     fieldCheck = ccsZisOptionChecked("ccs_vetting_type", "Select an option");
     if (fieldCheck !== true && fieldCheck !== undefined) errorStore.push(fieldCheck);
@@ -124,7 +123,6 @@ const ccsZvalidateRfPStrategy = event => {
       if (fieldCheck !== true) errorStore.push(fieldCheck);
     }
   }
-  console.log('errorrr ', errorStore[0])
   if (errorStore.length === 0) document.forms['ccs_rfp_exit_strategy_form'].submit();
   else ccsZPresentErrorSummary(errorStore);
 };
