29c6d5b6342aa7f0f31a9754ed85571cc6000c0f
CharunyaSP <102791086+CharunyaSP@users.noreply.github.com>
2022-06-23 20:13:39 +0530

    Update dashboard.ts

--- src/main/features/dashboard/controller/dashboard.ts
+++ src/main/features/dashboard/controller/dashboard.ts
@@ -3,9 +3,7 @@ import * as dashboarData from '../../../resources/content/dashboard/ccs-dashboar
 
 export const DASHBOARD = (req: express.Request, res: express.Response) => {
   req.session.unpublishedeventmanagement="false";
-  var today = new Date();
-  var ClosedDate = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
   // Active and Historical events is getting feached from API via 'src/main/common/middlewares/event-management/activeevents.ts'
-  const appendData = { data: dashboarData,ClosedDate, events: req.session.openProjectActiveEvents, historicalEvents: req.session.historicalEvents }
+  const appendData = { data: dashboarData, events: req.session.openProjectActiveEvents, historicalEvents: req.session.historicalEvents }
   res.render('dashboard', appendData);
 }
