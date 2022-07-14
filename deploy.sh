63d88fb9faf62790a6683b102f570edcb11822fc
aliab07 <106075211+aliab07@users.noreply.github.com>
2022-07-14 15:11:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@
 
 echo "Executing Terraform scripts in [$CF_ENVIRONMENT]"
 cd ./iac/environments/$CF_ENVIRONMENT/
+rm -rf .terraform
 terraform init -backend-config="bucket=$S3_STATE_BUCKET_NAME" -backend-config="dynamodb_table=$DDB_LOCK_TABLE_NAME"
 terraform validate
 terraform apply -auto-approve -input=false -var="cf_username=$CF_USERNAME" -var="cf_password=$CF_PASSWORD"
