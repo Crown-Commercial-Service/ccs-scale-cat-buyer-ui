763c1809d1009e9aeb64022e5ece4aad5b13a26f
Kumar <chandeshwar.kumar2@cognizant.com>
2022-07-15 19:24:53 +0530

    Added hide and show button for evaluation

--- src/main/public/assets/scripts/validations/validate_award.js
+++ src/main/public/assets/scripts/validations/validate_award.js
@@ -29,10 +29,10 @@ const ccsZvalidateStandStillPeriod = (event) => {
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  // if ($("#enter_evaluation_score_readOnly") !=undefined && $("#enter_evaluation_score_readOnly") !=null ) {
-  //   document.getElementById("enter_evaluation_score_readOnly").readOnly = true;
-  //   document.getElementById("enter_evaluation_feedback_readOnly").readOnly = true;
-  // }
+  if ($("#enter_evaluation_score_readOnly") !=undefined && $("#enter_evaluation_score_readOnly") !=null && $("#enter_evaluation_score_readOnly").length >0 ) {
+    document.getElementById("enter_evaluation_score_readOnly").readOnly = "true";
+    document.getElementById("enter_evaluation_feedback_readOnly").readOnly = "true";
+  }
     $('.event_managment_award').on('click', function (event) {
         event.preventDefault();
         const radioButtonYes = document.getElementById("standstill_period_yes").checked;
