557ada08777292e45c4117ae3874ed7ed276e122
SahitiVemulapalli <Sahiti.Vemulapalli@cognizant.com>
2022-08-03 13:18:29 +0530

    scat 5208,5015,5014

--- src/main/public/assets/scripts/validations/Message_reply.js
+++ src/main/public/assets/scripts/validations/Message_reply.js
@@ -6,6 +6,38 @@ const ccsZCountMsgReplyTextArea = (event) => {
       labelElement.innerText="You have "+count + " characters remaning of 5000";
    
   };
+  const ccsZCountQAQuestionTextArea = (event) => {
+    event.preventDefault();
+    const element = document.getElementById("QA_Question_input"); 
+      let labelElement=document.getElementById("QA_input_Question_count");
+      let count=5000-element.value.length;
+      labelElement.innerText="You have "+count + " characters remaning of 5000";
+   
+  };
+  const ccsZCountQAClarificationTextArea = (event) => {
+    event.preventDefault();
+    const element = document.getElementById("message_Add_Clerification_input"); 
+      let labelElement=document.getElementById("QA_input_clarification_count");
+      let count=5000-element.value.length;
+      labelElement.innerText="You have "+count + " characters remaning of 5000";
+   
+  };
+  const ccsZCountQAEditQuestionTextArea = (event) => {
+    event.preventDefault();
+    const element = document.getElementById("message_QA_Edit_Question_input"); 
+      let labelElement=document.getElementById("message_QA_Edit_Question_input_count");
+      let count=5000-element.value.length;
+      labelElement.innerText="You have "+count + " characters remaning of 5000";
+   
+  };
+  const ccsZCountQAEditClarificationTextArea = (event) => {
+    event.preventDefault();
+    const element = document.getElementById("message_QA_Edit_Answer_input"); 
+      let labelElement=document.getElementById("message_QA_Edit_Answer_input_count");
+      let count=5000-element.value.length;
+      labelElement.innerText="You have "+count + " characters remaning of 5000";
+   
+  };
   const ccsZCountMsgReplyTextbox = (event) => {
     event.preventDefault();
     const element = document.getElementById("reply_subject_input"); 
