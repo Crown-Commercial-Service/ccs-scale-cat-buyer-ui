fb48ffcd7af328df8f93e71bd1d8b3d9db3b1700
Kumar <chandeshwar.kumar2@cognizant.com>
2022-08-22 22:35:41 +0530

    end point testing

--- src/main/features/event-management/controller/eventManagement.ts
+++ src/main/features/event-management/controller/eventManagement.ts
@@ -71,7 +71,7 @@ export const EVENT_MANAGEMENT = async (req: express.Request, res: express.Respon
       });
 
       //#region supplier information
-      const baseurl_Supplier = `/agreements/${agreementId_session}/lots/${lotid}/suppliers`
+      const baseurl_Supplier = `/agreements/${agreementId_session}/lots/${lotid}/suppliersss`
       let supplierDataList = await (await AgreementAPI.Instance.get(baseurl_Supplier)).data;
       //#endregion
 
