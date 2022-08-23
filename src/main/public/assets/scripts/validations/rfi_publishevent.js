3ba4911f3eac7abd0cbde39cc64024664d6207f8
2073810-Sonam <101725800+2073810-Sonam@users.noreply.github.com>
2022-08-23 16:21:36 +0530

    SCAT-6124-1FC,2FC,DA-commit

--- src/main/public/assets/scripts/validations/rfi_publishevent.js
+++ src/main/public/assets/scripts/validations/rfi_publishevent.js
@@ -8,3 +8,14 @@ const ccsZRFIPublishEventSubmit = (event) => {
     }
     
   };
+
+  const ccsZRFPPublishEventSubmit = (event) => {
+    event.preventDefault();
+
+    let btn=document.getElementById('rfp_btn_publish_now')
+    if(btn!=null && btn!=undefined){
+        btn.disabled=true;
+    document.forms["ccs_rfp_publish_form"].submit();
+    }
+    
+  };
