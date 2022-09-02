fbcc1ea7ba5224672782ae7ffb67a9b2a0be9d73
Kumar <chandeshwar.kumar2@cognizant.com>
2022-09-02 11:23:47 +0530

    supplier code commit

--- src/main/features/event-management/controller/viewQA.ts
+++ src/main/features/event-management/controller/viewQA.ts
@@ -13,12 +13,23 @@ import * as inboxData from '../../../resources/content/event-management/qa.json'
  */
 export const EVENT_MANAGEMENT_QA = async (req: express.Request, res: express.Response) => {
     const { SESSION_ID } = req.cookies
-
+    const isSupplierQA = req.session['IsSupplierQA'];
+    let appendData : any ;
     try {
         res.locals.agreement_header = req.session.agreement_header
-        const baseURL = `/tenders/projects/${req.session.projectId}/events/${req.session.eventId}/q-and-a`;
-        const fetchData = await TenderApi.Instance(SESSION_ID).get(baseURL);
-        const appendData = { data: inboxData, QAs: fetchData.data, eventId: req.session['eventId'], eventType: req.session.eventManagement_eventType }
+        if(isSupplierQA !=undefined && isSupplierQA)
+        {
+            // supplier Qa end point 
+            const baseURL = `/tenders/projects/${req.session.projectId}/events/${req.session.eventId}/supplier/q-and-a`;
+            const fetchData = await TenderApi.Instance(SESSION_ID).get(baseURL);
+             appendData = { data: inboxData, QAs: fetchData.data, eventId: req.session['eventId'], eventType: req.session.eventManagement_eventType,isSupplierQA }
+        }
+        else 
+        {
+            const baseURL = `/tenders/projects/${req.session.projectId}/events/${req.session.eventId}/q-and-a`;
+            const fetchData = await TenderApi.Instance(SESSION_ID).get(baseURL);
+             appendData = { data: inboxData, QAs: fetchData.data, eventId: req.session['eventId'], eventType: req.session.eventManagement_eventType,isSupplierQA }
+        }
         res.render('viewQA', appendData)
     } catch (err) {
         LoggTracer.errorLogger(
@@ -34,5 +45,6 @@ export const EVENT_MANAGEMENT_QA = async (req: express.Request, res: express.Res
 }
 
 export const EVENT_MANAGEMENT_SUPPLIER_QA = async (req: express.Request, res: express.Response) => {
+    req.session['IsSupplierQA'] = true;
     res.redirect('/event/qa');
 }
