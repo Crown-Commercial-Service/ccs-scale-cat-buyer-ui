29f9f18d1d97f2cc7a57b2f1fd4fbc021d06afd4
Balwinder.Singh <Balwinder.Singh@matthey.com>
2022-09-27 19:44:39 +0530

    commit

--- src/main/features/event-management/controller/viewQA.ts
+++ src/main/features/event-management/controller/viewQA.ts
@@ -45,7 +45,7 @@ export const EVENT_MANAGEMENT_SUPPLIER_QA = async (req: express.Request, res: ex
         const baseURL = `/tenders/projects/${projectId}/events/${eventId}/q-and-a`;
         const fetchData = await TenderApi.Instance(SESSION_ID).get(baseURL);
         if (fetchData != undefined && fetchData != null && fetchData.data.QandA?.length > 0) {
-            res.locals.agreement_header = { projectId: projectId, eventId: eventId, project_name: fetchData.data.projectName , agreementName: fetchData.data.agreementName, agreementId_session: fetchData.data.agreementId, agreementLotName: "", lotid: fetchData.data.lotId }
+            res.locals.agreement_header = { projectId: projectId, eventId: eventId, project_name: fetchData.data.projectName , agreementName: fetchData.data.agreementName, agreementId_session: fetchData.data.agreementId, agreementLotName: fetchData.data.lotName, lotid: fetchData.data.lotId }
             req.session.agreement_header = res.locals.agreement_header
             appendData = { data: inboxData, QAs: fetchData.data.QandA, eventId: eventId, eventType: req.session.eventManagement_eventType, isSupplierQA: true }
         }
