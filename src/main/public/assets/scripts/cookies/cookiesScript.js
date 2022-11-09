c30781cd7eedd9b0d3f2103db210aa8e2701d54e
leochelliah-bc <leo.chelliah@brickendon.com>
2022-11-09 12:12:43 +0000

    Hotfix: updating the google tag manager changes

--- src/main/public/assets/scripts/cookies/cookiesScript.js
+++ src/main/public/assets/scripts/cookies/cookiesScript.js
@@ -147,7 +147,7 @@
             j.async = true;
             j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
             f.parentNode.insertBefore(j, f);
-        })(window, document, "script", "dataLayer", "GTM-5NQGRQN");
+        })(window, document, "script", "dataLayer", "GTM-NZFDQHB");
     }
     function updateSeenCookie() {
         // 1 year = 3.154e+7
