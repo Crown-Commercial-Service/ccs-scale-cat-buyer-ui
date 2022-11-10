e165f5e0ad0029d0161ff11115a94e2676cc61cb
abijith.v <abijith.venugopalan@dreamguystech.com>
2022-11-10 18:16:06 +0530

    SCAT-7479 D274 values fixed

--- src/main/public/assets/scripts/validations/rfp_select.js
+++ src/main/public/assets/scripts/validations/rfp_select.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
             let maxBudget = Number(inputArray[0].value);
             let msg = '';
             if ((maxBudget == '') || (maxBudget == 0)) {
-              msg = 'You must enter a values';
+              msg = 'You must enter a value';
             } else if (maxBudget > 0 && (maxBudget < minBudget) || (maxBudget == minBudget)) {
               msg = 'Maximum budget must be greater than minimum budget';
             } 
