ac7e250dfb8be8a61649def1601faa9e6d001247
ananthakumar-bc <anantha.muthaiya@brickendon.com>
2022-11-12 13:52:59 +0530

    SCAT-6304 upload document journey status updated

--- src/main/features/requirements/controller/rfp-uploadOverview.ts
+++ src/main/features/requirements/controller/rfp-uploadOverview.ts
@@ -6,7 +6,7 @@ import { TenderApi } from './../../../common/util/fetch/procurementService/Tende
 import { TokenDecoder } from '../../../common/tokendecoder/tokendecoder';
 import { LoggTracer } from '../../../common/logtracer/tracer';
 import {ShouldEventStatusBeUpdated} from '../../shared/ShouldEventStatusBeUpdated';
-
+import { DynamicFrameworkInstance } from '../util/fetch/dyanmicframeworkInstance';
 /**
  *
  * @Rediect
@@ -28,12 +28,34 @@ export const RFP_UPLOAD = async (req: express.Request, res: express.Response) =>
   req.session['isJaggaerError'] = false;
   res.locals.agreement_header = { agreementName, project_name, agreementId_session, agreementLotName, lotid };
   
+    
+    const FILE_PUBLISHER_BASEURL = `/tenders/projects/${projectId}/events/${eventId}/documents`;
+    const FetchDocuments = await DynamicFrameworkInstance.Instance(SESSION_ID).get(FILE_PUBLISHER_BASEURL);
+    const FETCH_FILEDATA = FetchDocuments?.data;
+    
+   Mcf3uploadData.taskList[0].taskStatus="TO DO";
+   Mcf3uploadData.taskList[1].taskStatus="TO DO";
+   Mcf3uploadData.taskList[2].taskStatus="Optional";
+    FETCH_FILEDATA?.map(file => {
+      
+      if (file.description === "mandatoryfirst") {
+        Mcf3uploadData.taskList[0].taskStatus="Done";
+      }
+      if (file.description === "mandatorysecond") {
+        Mcf3uploadData.taskList[1].taskStatus="Done";
+      }
+      if (file.description === "optional") {
+        Mcf3uploadData.taskList[2].taskStatus="Done";
+      }
+    });
+    
     let forceChangeDataJson;
     if(agreementId_session == 'RM6187') { //MCF3
       forceChangeDataJson = Mcf3uploadData;
     } else { 
       forceChangeDataJson = uploadData;
     }
+
   const appendData = { data: forceChangeDataJson, releatedContent, error: isJaggaerError, agreementId_session };
   try {
     let flag=await ShouldEventStatusBeUpdated(eventId,30,req);
