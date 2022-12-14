57951c93f355078e1f352ebb479c033fbdf2700a
Gobika-bc <gobika.brickendon@gmail.com>
2022-12-14 11:28:07 +0530

    SCAT-6570:Validation content fixed

--- src/main/public/assets/scripts/validations/validate_rfp_strategy.js
+++ src/main/public/assets/scripts/validations/validate_rfp_strategy.js
@@ -171,7 +171,7 @@ const ccsZvalidateRfPStrategy = event => {
   if ($('#rfp_prob_statement_s') !== undefined && $('#rfp_prob_statement_s').val() !== undefined && !pageHeading.includes("(Optional)") && pageHeading.includes('Management information and reporting requirements')&& agreement_id === "RM6187") {
     if ($('#rfp_prob_statement_s').val().length === 0) {
 
-      fieldCheck = ccsZvalidateTextArea('rfp_prob_statement_s', 'You must enter your information and requirements');
+      fieldCheck = ccsZvalidateTextArea('rfp_prob_statement_s', 'You must add Management information and reporting requirements');
       if (fieldCheck !== true) errorStore.push(fieldCheck);
     }
   }
