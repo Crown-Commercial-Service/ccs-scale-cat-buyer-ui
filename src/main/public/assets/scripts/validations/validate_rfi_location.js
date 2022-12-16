94ddd3dbdee139a2362813ba8162d873a8baf9e3
ananthakumar-bc <anantha.muthaiya@brickendon.com>
2022-12-16 15:12:54 +0530

    SCAT-7397 multiple location selected

--- src/main/public/assets/scripts/validations/validate_rfi_location.js
+++ src/main/public/assets/scripts/validations/validate_rfi_location.js
@@ -13,32 +13,49 @@ document.addEventListener('DOMContentLoaded', () => {
         noLocationtagid = cl.id;
         // console.log('va from rfi onload>>>>',noLocationtagid);
       }
+      if(cl.value === "Overseas") {
+        overSeesid = cl.id;
+        // console.log('va from rfi onload>>>>',noLocationtagid);
+      }
       
+
     })
+   
+    let allCheckboxOverSees = document.getElementById(overSeesid);
+
     let allCheckbox = document.getElementById(noLocationtagid),
       locationCheckboxes = document.querySelectorAll("input[name='required_locations']");
 
     if (allCheckbox.checked) {
       locationCheckboxes.forEach((cb) => {
-
-        if (cb.value != allCheckbox.value) cb.disabled = true;
+        if (cb.value != allCheckbox.value && cb.value != allCheckboxOverSees.value) {
+         // cb.disabled = true;
+        }
+        
 
       });
     }
 
     document.getElementById(noLocationtagid).addEventListener('change', () => {
+      let alloverSeesid = document.getElementById(overSeesid);
+
       let allCb = document.getElementById(noLocationtagid),
         locationCheckboxes = document.querySelectorAll("input[name='required_locations']");
       // console.log('va from rfi>>>>');
       locationCheckboxes.forEach((cb) => {
 
-        if (allCb.checked && cb.value != allCb.value) {
-          cb.checked = false;
-          cb.disabled = true;
+        if (allCb.checked && cb.value != allCb.value && cb.value != alloverSeesid.value) {
+    
+        //    cb.checked = false;
+        //  cb.disabled = true;
         }
 
         if (!allCb.checked && cb.value != allCb.value) {
-          cb.disabled = false;
+          //cb.disabled = false;
+        }
+
+        if (!allCb.checked && cb.value != alloverSeesid.value) {
+          //cb.disabled = false;
         }
 
       });
