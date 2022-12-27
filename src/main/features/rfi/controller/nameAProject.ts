e39b7d1d88ea5b41cfda76a21e6158f12a7f1652
ananthakumar-bc <anantha.muthaiya@brickendon.com>
2022-12-27 10:26:56 +0530

    logerExceptionStructureUpdated

--- src/main/features/rfi/controller/nameAProject.ts
+++ src/main/features/rfi/controller/nameAProject.ts
@@ -67,8 +67,7 @@ export const POST_NAME_PROJECT = async (req: express.Request, res: express.Respo
   try {
     if (name) {
       const _body = {
-        dataid: "asd sd",
-        nameSecond: name,
+        name: name,
       };
       const response = await TenderApi.Instance(SESSION_ID).put(nameUpdateUrl, _body);
       const response2 = await TenderApi.Instance(SESSION_ID).put(eventUpdateUrl, _body);
@@ -80,7 +79,6 @@ export const POST_NAME_PROJECT = async (req: express.Request, res: express.Respo
       res.redirect('/rfi/name-your-project');
     }
   } catch (error) {
-    
     LoggTracer.errorLogger(
       res,
       error,
