7420272e0572eacd5a94c9345d6b96cd026c20ba
BalajiRamasamy-bc <balaji.brickendon@gmail.com>
2023-01-02 13:34:14 +0530

    collaborator and context file completed

--- src/main/common/logtracer/logConstant.ts
+++ src/main/common/logtracer/logConstant.ts
@@ -6,4 +6,9 @@ export enum logConstant {
     chooseCommercialLandLog = 'Choose a commercial agreement page',
     aggrementDetailFetch = 'Feached agreement details from Agreement service API',
     lotDetailsFromAggrement = 'Feached Lot details from Agreement service API',
+    collaboratorDetailFetch = 'Feached collaborator details from Organization service API',
+    userDetailFetch = 'Feached user details from Tender API',
+    collaboratorSave = 'Saved collaborator from Tender API',
+    collaboratorDelete = 'Delete collaborator from Tender API',
+    criteriaDetailFetch='Feached criteria details from Tender API'
 }
