987aa6c230803ebbae487a1711f8d45984b792a0
InbarasanRaju-bc <inbarasan.brickendon@gmail.com>
2023-01-02 13:20:13 +0530

    Pre marketing page middleware log enabled

--- src/main/common/logtracer/logConstant.ts
+++ src/main/common/logtracer/logConstant.ts
@@ -2,8 +2,12 @@ export enum logConstant {
     //@ All of the log message content related
     loginSuccess = 'Login successfully',
     logoutSuccess = 'Logout successfully',
-    dashLandLog = 'Dashboard landing page',
-    chooseCommercialLandLog = 'Choose a commercial agreement page',
+    dashLandLog = 'Dashboard page reached',
+    chooseCommercialLandLog = 'Choose a commercial agreement page reached',
     aggrementDetailFetch = 'Feached agreement details from Agreement service API',
     lotDetailsFromAggrement = 'Feached Lot details from Agreement service API',
+    evenTypeFromAggrementLot = 'Feached event types from specific Agreement lot service API',
+    procurementCreated = 'Procurement created',
+    procurementPage = 'Procurement page reached',
+    postSaveEventsToProject = 'Selected events update to the project',
 }
