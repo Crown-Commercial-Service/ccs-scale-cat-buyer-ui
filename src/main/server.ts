17da24f0a363afd6f54afee4ee582dfeec61d3db
InbarasanRaju-bc <inbarasan.brickendon@gmail.com>
2023-01-18 17:03:05 +0530

    SCAT-8466: Server Timeout implemented

--- src/main/server.ts
+++ src/main/server.ts
@@ -24,10 +24,10 @@ if (app.locals.ENV === 'development') {
   const ELB = app.listen(port, () => {
     logger.info(`Application started: http://localhost:${port}`);
   });
-  // ELB.setTimeout(0);
-  // ELB.keepAliveTimeout = 60 * 100000;
-  // ELB.headersTimeout = 61 * 100000;
-  // ELB.requestTimeout = 61 * 100000;
+  ELB.setTimeout(0);
+  ELB.keepAliveTimeout = 60 * 100000;
+  ELB.headersTimeout = 61 * 100000;
+  ELB.requestTimeout = 61 * 100000;
   ELB.on('connection', function(socket) {
     console.log("A new connection was made by a client.");
     socket.setTimeout(30 * 100000); 
