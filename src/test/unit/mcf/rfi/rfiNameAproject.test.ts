6b761c74374a17dfa0c4190c69521c2b13c68d8a
ananthakumar-bc <anantha.muthaiya@brickendon.com>
2023-01-23 15:00:32 +0530

    SCAT-8502 :RFI Name a project unit test case

---
+++ src/test/unit/mcf/rfi/rfiNameAproject.test.ts
@@ -0,0 +1,67 @@
+//@ts-nocheck
+
+import { expect } from 'chai';
+import request from 'supertest';
+let chais = require("chai");
+let chaiHttp = require("chai-http");
+import { app } from '../../../../main/app';
+import nock from 'nock';
+import express from 'express';
+import { createDummyJwt } from 'test/utils/auth';
+const environentVar = require('dotenv').config();
+const { parsed: envs } = environentVar;
+import { JSDOM } from 'jsdom';
+import { getToken } from 'test/utils/getToken';
+import  mcfData from '../../../data/mcf/rfi/rfiNameAproject.json';
+
+chais.should();
+chais.use(chaiHttp);
+
+describe('MCF3: Name a project', async () => {
+  let parentApp;
+  let OauthToken;
+  // let procid=mcfData.procurements.procurementID;
+  // let procid=mcfData.procurements.eventId;
+  
+  // console.log("procid",procid);
+  // console.log("eventId",eventId);
+
+  beforeEach(async function () {
+    OauthToken = await getToken();
+    parentApp = express();
+    parentApp.use(function (req, res, next) {
+    req.session = mcfData
+    req.session.access_token=OauthToken;    
+      next();
+    });
+    parentApp.use(app);
+  });
+
+  it('should render `MCF3 Name a project ` page when everything is fine', async () => {
+    await request(parentApp)
+      .get('/rfi/name-your-project')
+      .set('Cookie', [`SESSION_ID=${OauthToken}`, 'state=blah'])
+      .expect(res => {
+        expect(res.status).to.equal(200);
+      });
+  });
+
+  it('should redirect to procurement lead if name fulfilled', async () => {
+    const dummyName = 'test';
+    
+    let procId = mcfData.procurements[0].procurementID;
+    let eventId = mcfData.procurements[0].eventId;
+    
+    nock(envs.TENDERS_SERVICE_API_URL).put(`/tenders/projects/${procId}/name`).reply(200, true);
+    nock(envs.TENDERS_SERVICE_API_URL).put(`/tenders/projects/${procId}/events/${eventId}`).reply(200, true);
+    await request(parentApp)
+      .post(`/rfi/name?procid=${procId}`)
+      .send({ name: dummyName })
+      .set('Cookie', [`SESSION_ID=${OauthToken}`, 'state=blah'])
+      .expect(res => {
+       expect(res.status).to.equal(302);
+        
+      });
+  });
+
+});
