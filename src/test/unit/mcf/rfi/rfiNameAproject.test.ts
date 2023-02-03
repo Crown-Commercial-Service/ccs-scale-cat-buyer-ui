daae37c361d95b1f7beb4f4aef1e59a07048a592
GowriRamasamy-bc <Gowri.Ramasamy@brickendon.com>
2023-02-03 16:38:35 +0530

    SCAT-8589 :Gclould RFI Unit Test Case

--- src/test/unit/mcf/rfi/rfiNameAproject.test.ts
+++ src/test/unit/mcf/rfi/rfiNameAproject.test.ts
@@ -12,6 +12,7 @@ const environentVar = require('dotenv').config();
 const { parsed: envs } = environentVar;
 import { JSDOM } from 'jsdom';
 import { getToken } from 'test/utils/getToken';
+//import  mcfData from '../../../data/mcf/rfi/rfiJsonFormet.json';
 import  mcfData from '../../../data/mcf/rfi/rfiJsonFormet.json';
 
 chais.should();
