c8d07043324812dfb29e13c400b2acdaa8384cad
Mohamedmohaseen-bc <mohamed.malik@brickendon.com>
2023-03-28 19:18:43 +0530

    CAS-1070 : Validation message updated

--- src/main/public/assets/scripts/bundle.js
+++ src/main/public/assets/scripts/bundle.js
@@ -18978,7 +18978,7 @@ const emptyFieldCheckRfpKPI = (type_base='') => {
           if (definition_field.value.trim() == ''){
             errField = definition_field;
              isErrorSingle = true;
-            ccsZaddErrorMessage(definition_field, 'You must enter the description of the criteria');
+            ccsZaddErrorMessage(definition_field, 'Enter the description of the criteria');
           }
           if (target_field !== undefined && target_field !== null && target_field.value.trim() === '') {
             ccsZaddErrorMessage(target_field, 'Enter a success target between 1 and 100');
@@ -19005,11 +19005,11 @@ const emptyFieldCheckRfpKPI = (type_base='') => {
           if (term_field.value.trim() === '' && definition_field.value.trim() === '' && target_field.value.trim() === '') {
             fieldCheck = [definition_field.id, 'You must add information in all fields'];
             ccsZaddErrorMessage(term_field, 'You must enter the name of requirement');
-            ccsZaddErrorMessage(definition_field, 'You must enter the description of the criteria');
+            ccsZaddErrorMessage(definition_field, 'Enter the description of the criteria');
             ccsZaddErrorMessage(target_field, 'Enter a success target between 1 and 100');
             fieldCheck = ['rfp_term_service_levels_KPI_' + x, 'You must enter the name of requirement'];
             errorStore.push(fieldCheck);
-            fieldCheck = ["rfp_term_definition_service_levels_KPI_" + x, 'You must enter the description of the criteria'];
+            fieldCheck = ["rfp_term_definition_service_levels_KPI_" + x, 'Enter the description of the criteria'];
             errorStore.push(fieldCheck);
             fieldCheck = ["rfp_term_percentage_KPI_" + x, 'Enter a success target between 1 and 100'];
             errorStore.push(fieldCheck);
@@ -19023,9 +19023,9 @@ const emptyFieldCheckRfpKPI = (type_base='') => {
               errorStore.push(fieldCheck);
             }
             if (definition_field.value.trim() === '') {
-              ccsZaddErrorMessage(definition_field, 'You must enter the description of the criteria');
+              ccsZaddErrorMessage(definition_field, 'Enter the description of the criteria');
               isError = true;
-              fieldCheck = ["rfp_term_definition_service_levels_KPI_" + x, 'You must enter the description of the criteria'];
+              fieldCheck = ["rfp_term_definition_service_levels_KPI_" + x, 'Enter the description of the criteria'];
               errorStore.push(fieldCheck);
             }
 
