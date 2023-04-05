78a095cdbd3477ce5611544d8cd57a76da540585
kamalakkannan <kamalakkannan.rajaram@brickendon.com>
2023-04-05 16:30:03 +0530

    CAS-1074 : MCF3- FC/DA- Tell us if there an existing supplier validation removed

--- src/main/public/assets/scripts/bundle.js
+++ src/main/public/assets/scripts/bundle.js
@@ -9769,20 +9769,20 @@ const ccsZvalidateEoiSecurity = (event) => {
   
   fieldCheck = ccsZisOptionChecked("ccs_vetting_type", msg);
   if (fieldCheck !== true) errorStore.push(fieldCheck);
-  if ($('#rfp_security_confirmation') !== undefined && $('#rfp_security_confirmation').val() !== undefined && $("input[name='ccs_vetting_type']").prop('checked')) {
+  //if ($('#rfp_security_confirmation') !== undefined && $('#rfp_security_confirmation').val() !== undefined && $("input[name='ccs_vetting_type']").prop('checked')) {
     // errorStore.length = 0;
     
-    if ($('#rfp_security_confirmation').val().length === 0) {      
-      if(urlParams.get('agreement_id') == 'RM6187')
-           fieldCheck = ccsZvalidateTextArea('rfp_security_confirmation', 'You have the option to enter the name of an existing supplier.');
-      else fieldCheck = ccsZvalidateTextArea('rfp_security_confirmation', 'Provide the name of the incumbent supplier');
-      if (fieldCheck !== true) errorStore.push(fieldCheck);
-    }
-    else if ($('#rfp_security_confirmation').val().length > 500) {
-      fieldCheck = ccsZvalidateTextArea('rfp_security_confirmation', 'supplier name must be less than 500 characters');
-      if (fieldCheck !== true) errorStore.push(fieldCheck);
-    }
-  }
+    // if ($('#rfp_security_confirmation').val().length === 0) {      
+    //   if(urlParams.get('agreement_id') == 'RM6187')
+    //        fieldCheck = ccsZvalidateTextArea('rfp_security_confirmation', 'You have the option to enter the name of an existing supplier.');
+    //   else fieldCheck = ccsZvalidateTextArea('rfp_security_confirmation', 'Provide the name of the incumbent supplier');
+    //   if (fieldCheck !== true) errorStore.push(fieldCheck);
+    // }
+    // else if ($('#rfp_security_confirmation').val().length > 500) {
+    //   fieldCheck = ccsZvalidateTextArea('rfp_security_confirmation', 'supplier name must be less than 500 characters');
+    //   if (fieldCheck !== true) errorStore.push(fieldCheck);
+    // }
+  //}
 
   if (errorStore.length === 0) document.forms["ccs_eoi_vetting_form"].submit();
   else ccsZPresentErrorSummary(errorStore);
@@ -18660,10 +18660,12 @@ $('#rfp_singleselect').on('submit', event => {
     ) {
       ccsZaddErrorMessage(rfp_security_confirmation, 'Please enter only character.');
       ccsZPresentErrorSummary([['rfp_security_confirmation', 'Please enter only character.']]);
-    } else if (ccs_vetting_type == true && $('#rfp_security_confirmation').val().length === 0 && document.getElementById('agreementID').value != 'RM1557.13') {
-      ccsZaddErrorMessage(rfp_security_confirmation, 'You have the option to enter the name of an existing supplier.');
-      ccsZPresentErrorSummary([['rfp_security_confirmation', 'You have the option to enter the name of an existing supplier.']]);
-    } else {
+    } 
+    // else if (ccs_vetting_type == true && $('#rfp_security_confirmation').val().length === 0 && document.getElementById('agreementID').value != 'RM1557.13') {
+    //   ccsZaddErrorMessage(rfp_security_confirmation, 'You have the option to enter the name of an existing supplier.');
+    //   ccsZPresentErrorSummary([['rfp_security_confirmation', 'You have the option to enter the name of an existing supplier.']]);
+    // } 
+    else {
       document.forms['rfp_singleselect'].submit();
     }
   } else {
