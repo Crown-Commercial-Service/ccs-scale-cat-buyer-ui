34467d615fc22457182dc83c7a4ea98f4c7eb81f
Mohamedmohaseen-bc <mohamed.malik@brickendon.com>
2023-05-04 12:19:11 +0530

    CAS-1233 : Button enabled for no score

--- src/main/features/event-management/controller/evaluateSuppliers.ts
+++ src/main/features/event-management/controller/evaluateSuppliers.ts
@@ -189,6 +189,11 @@ export const EVALUATE_SUPPLIERS = async (req: express.Request, res: express.Resp
     if(stage2_data.length > 0){
       stage2_value = 'Stage 2';
     }
+    console.log("ConfirmFlag",ConfirmFlag);
+    console.log("CountConfirmFlag",CountConfirmFlag);
+    console.log("checkcount",checkcount);
+    console.log("count",count);
+    
     
     //if (status == "Published" || status == "Response period closed" || status == "Response period open" || status=="To be evaluated" ) {
           const appendData = { releatedContent,agreement_header,agreementId_session,lotid,ConfirmFlag,CountConfirmFlag,checkcount,ScoresAndFeedbackURLdata,data: eventManagementData,eventId, supplierName, supplierSummary, showallDownload, suppliers: localData ,stage2_value }
