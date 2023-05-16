5b6b5fcbc4309039c45bbe953d23f7cb757bac0b
anandbusinessaccount <126943175+anandbusinessaccount@users.noreply.github.com>
2023-05-16 17:13:10 +0530

    CAS-1278 API Comment removed

--- src/main/features/event-management/controller/eventManagement.ts
+++ src/main/features/event-management/controller/eventManagement.ts
@@ -345,10 +345,10 @@ export const EVENT_MANAGEMENT = async (req: express.Request, res: express.Respon
       fetchData='';
       qasCount = 0;
     }else{
-    //  const baseQandAURL = `/tenders/projects/${req.session.projectId}/events/${req.session.eventId}/q-and-a`;
+      const baseQandAURL = `/tenders/projects/${req.session.projectId}/events/${req.session.eventId}/q-and-a`;
      
-    //  fetchData = await TenderApi.Instance(SESSION_ID).get(baseQandAURL);
-      fetchData = [];
+      fetchData = await TenderApi.Instance(SESSION_ID).get(baseQandAURL);
+     
       //CAS-INFO-LOG 
       LoggTracer.infoLogger(fetchData, logConstant.getQuestionAndAnsDetails, req);
       if (fetchData?.data != undefined) {
@@ -926,10 +926,9 @@ export const EVENT_MANAGEMENT_CLOSE = async (req: express.Request, res: express.
       }
      
       //Get Q&A Count
-     // const baseQandAURL = `/tenders/projects/${req.session.projectId}/events/${req.session.eventId}/q-and-a`;
+      const baseQandAURL = `/tenders/projects/${req.session.projectId}/events/${req.session.eventId}/q-and-a`;
      
-     // const fetchData = await TenderApi.Instance(SESSION_ID).get(baseQandAURL);
-     const fetchData = [];
+      const fetchData = await TenderApi.Instance(SESSION_ID).get(baseQandAURL);
       let qasCount = 0;
       if (fetchData.data != undefined) {
         qasCount = fetchData.data.QandA;
