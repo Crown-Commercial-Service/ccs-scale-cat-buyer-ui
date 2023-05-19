de45ee379739da603252cc1771a554c722657741
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2023-05-19 16:20:41 +0100

    I have removed express-nunjucks as we do not use it so don’t really need it as a dependency.
    In addition, I’ve tidied up how we initialise Nunjucks so that it is more clear what is happening. Any additional changes are caused by VSCode formatting/linting the file when I save.
    
    I’ve also updated the initialisation of Helmet too as just creating a new class instance and not assigning it is a strange thing to do as GC will just clear it up. Therefore, I’ve now made it functional.

--- src/main/modules/helmet/index.ts
+++ src/main/modules/helmet/index.ts
@@ -1,49 +1,43 @@
-import * as express from 'express';
-import helmet = require('helmet');
+import { Application } from 'express';
+import helmet from 'helmet';
 
-export interface HelmetConfig {
+interface HelmetConfig {
   referrerPolicy: string;
 }
 
 const googleAnalyticsDomain = '*.google-analytics.com';
 const googleAnalyticsGtm = '*.googletagmanager.com';
-const self = "'self'";
+const self = '\'self\'';
 
-/**
- * Module that enables helmet in the application
- */
-export class Helmet {
-  constructor(public config: HelmetConfig) {}
+const setContentSecurityPolicy = (app: Application): void => {
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        connectSrc: [self, googleAnalyticsDomain, 'https://stats.g.doubleclick.net'],
+        defaultSrc: ['\'none\''],
+        fontSrc: [self, 'data:'],
+        imgSrc: [self, googleAnalyticsDomain, googleAnalyticsGtm],
+        objectSrc: [self],
+        scriptSrc: [self,'https://snap.licdn.com', googleAnalyticsDomain, '\'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU=\'', '\'sha256-yJD+OaJ97T/y5QHr3Se9W0AW2baOVjc1FR3Na10Ih+Y=\'','\'sha256-Zk3m64g3XkpEOa4ilYFv4J6vnDXIEsOJMYonokV/R6E=\'', '\'sha256-zEF/ALwwDYV2nZ+rdYGh2XpjU1lbO3oZ2osZayOlmpw=\'', googleAnalyticsGtm, '\'sha256-xB3p3jeZFuTiApvSQLM9BrzAfwNP7gzCnJPZ4VRVpCw=\''],
+        styleSrc: [self],
+      },
+    }),
+  );
+};
 
-  public enableFor(app: express.Express): void {
-    // include default helmet functions
-    app.use(helmet());
-
-    this.setContentSecurityPolicy(app);
-    this.setReferrerPolicy(app, this.config.referrerPolicy);
+const setReferrerPolicy = (app: Application, policy: string): void => {
+  if (!policy) {
+    throw new Error('Referrer policy configuration is required');
   }
 
-  private setContentSecurityPolicy(app: express.Express): void {
-    app.use(
-      helmet.contentSecurityPolicy({
-        directives: {
-          connectSrc: [self, googleAnalyticsDomain, 'https://stats.g.doubleclick.net'],
-          defaultSrc: ["'none'"],
-          fontSrc: [self, 'data:'],
-          imgSrc: [self, googleAnalyticsDomain, googleAnalyticsGtm],
-          objectSrc: [self],
-          scriptSrc: [self,'https://snap.licdn.com', googleAnalyticsDomain, "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='", "'sha256-yJD+OaJ97T/y5QHr3Se9W0AW2baOVjc1FR3Na10Ih+Y='","'sha256-Zk3m64g3XkpEOa4ilYFv4J6vnDXIEsOJMYonokV/R6E='", "'sha256-zEF/ALwwDYV2nZ+rdYGh2XpjU1lbO3oZ2osZayOlmpw='", googleAnalyticsGtm, "'sha256-xB3p3jeZFuTiApvSQLM9BrzAfwNP7gzCnJPZ4VRVpCw='"],
-          styleSrc: [self],
-        },
-      }),
-    );
-  }
+  app.use(helmet.referrerPolicy({ policy }));
+};
 
-  private setReferrerPolicy(app: express.Express, policy: string): void {
-    if (!policy) {
-      throw new Error('Referrer policy configuration is required');
-    }
+const initHelmet = (app: Application, config: HelmetConfig): void => {
+  app.use(helmet());
 
-    app.use(helmet.referrerPolicy({ policy }));
-  }
-}
+  setContentSecurityPolicy(app);
+  setReferrerPolicy(app, config.referrerPolicy);
+};
+
+export { initHelmet };
