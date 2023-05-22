5797ff2004d9502ed88561c5a17df7a77dab5567
ananthakumar-bc <anantha.muthaiya@brickendon.com>
2023-05-22 19:48:51 +0530

    CAS-897 Standstile and presentaion Yes or no desing integrated in FC

--- src/main/assets/scripts/validations/rfi_responsedate.js
+++ src/main/assets/scripts/validations/rfi_responsedate.js
@@ -14,16 +14,33 @@ for (const selector of totalElementSelectors) {
 }
 
 
+
 for (const selector of totalElementSelectors) {
+    // headBodyContent_4 #resdateradio4
+
+    
+    let elementRadioID = ".resdateradioclass" + selector;
+    let elementRadioIDSelector = $(elementRadioID);
+    if(selector==6 || selector==7){
+        $('#headBodyContent_'+selector).hide();
+    }else{
+        $('#headContent_'+selector).hide();
+        
+    }
+   
+    console.log("elementRadioIDSelector",elementRadioIDSelector);
+    console.log("selector",selector);
+
     let elementID = "#change_clarification_date_" + selector;
     let elementCancelID = "#cancel_change_clarification_date_" + selector;
     let elementSelector = $(elementID);
     let elementSelectorCancel = $(elementCancelID);
     elementSelector.fadeIn();
+
     elementSelectorCancel.fadeIn();
     elementSelector.on('click', () => {
         removeErrorFieldsEoiTerms();
-       
+        console.log("test1")
         localStorage.removeItem('dateItem');
         localStorage.setItem('dateItem', elementSelector.selector);
         let ClickedID = "#rfi_clarification_date_expanded_" + selector;
@@ -39,11 +56,32 @@ for (const selector of totalElementSelectors) {
         elementSelector.hide();
         saveButtonHideDateRfi();
     })
+
+    //Radio Yes or no Select
+    elementRadioIDSelector.on('click', () => {
+        let getClassName = 'resdateradio'+selector;
+
+        let checkedBox = $('input[name='+getClassName+']:checked').val();
+
+        if(checkedBox=='yes'){
+            console.log("yess")
+           $('#headBodyContent_'+selector).show();
+           $('#headContent_'+selector).removeClass('hidehr');
+        }
+        if(checkedBox=='no'){
+            console.log("NOOoo")
+           $('#headBodyContent_'+selector).hide();
+           $('#headContent_'+selector).addClass('hidehr');
+        }
+         console.log("checkedBox",checkedBox);
+
+        })
+
 }
 var errorSelectorId = '';
 let errorSelector = $("#click-error");
 errorSelector.on('click', () => {
-    
+    console.log("test2")
     let ClickedID = $("#click-error").attr("href");
     let errorText = $("#click-error").text();
     errorSelectorId = ClickedID;
@@ -59,7 +97,7 @@ for (const selector of totalElementSelectors) {
     let elementID = "#cancel_change_clarification_date_" + selector;
     let elementSelector = $(elementID);
     elementSelector.on('click', () => {
-
+        console.log("test3")
         let ClickedID = "#rfi_clarification_date_expanded_" + selector;
 
         let elementSelectorClicked = $(ClickedID);
@@ -91,3 +129,10 @@ const saveButtonHideDateRfi = () => {
 const saveButtonUnHideDateRfi = () => {
     document.getElementById("hideMeWhileDateChange").disabled = false;
 }
+
+// function myfunction(index) {
+//     console.log(index)
+// }
+// document.querySelectorAll("input[name='resdateradio']").forEach((input) => {
+//     input.addEventListener('change', myfunction);
+// });
