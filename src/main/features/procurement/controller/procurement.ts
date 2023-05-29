ad17b8871d5e14dd33e522dd58da6113171aaf6a
Mohamedmohaseen-bc <mohamed.malik@brickendon.com>
2023-05-29 12:47:07 +0530

    Console remove

--- src/main/features/procurement/controller/procurement.ts
+++ src/main/features/procurement/controller/procurement.ts
@@ -270,7 +270,6 @@ export const PROCUREMENT = async (req: express.Request, res: express.Response) =
     }
       
   } catch (error) {
-    console.log(error);
     
     LoggTracer.errorLogger(
       res,
