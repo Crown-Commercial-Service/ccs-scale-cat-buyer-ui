b390985cb4e2caf60fd4e3449b300d745a4f28e5
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2023-07-10 12:49:55 +0100

    Update the helmet config

--- src/main/modules/helmet/index.ts
+++ src/main/modules/helmet/index.ts
@@ -1,53 +1,52 @@
 import { Application } from 'express';
 import helmet from 'helmet';
 
-interface HelmetConfig {
-  referrerPolicy: string;
-}
-
 const googleAnalyticsDomain = '*.google-analytics.com';
 const googleAnalyticsGtm = '*.googletagmanager.com';
 const self = '\'self\'';
 
 const setContentSecurityPolicy = (app: Application): void => {
   app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        connectSrc: [self, googleAnalyticsDomain, 'https://stats.g.doubleclick.net'],
-        defaultSrc: ['\'none\''],
-        fontSrc: [self, 'data:'],
-        imgSrc: [self, googleAnalyticsDomain, googleAnalyticsGtm],
-        objectSrc: [self],
-        scriptSrc: [
-          self,
-          'https://snap.licdn.com',
-          googleAnalyticsDomain,
-          '\'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU=\'',
-          '\'sha256-yJD+OaJ97T/y5QHr3Se9W0AW2baOVjc1FR3Na10Ih+Y=\'',
-          '\'sha256-Zk3m64g3XkpEOa4ilYFv4J6vnDXIEsOJMYonokV/R6E=\'',
-          '\'sha256-zEF/ALwwDYV2nZ+rdYGh2XpjU1lbO3oZ2osZayOlmpw=\'',
-          googleAnalyticsGtm,
-          '\'sha256-xB3p3jeZFuTiApvSQLM9BrzAfwNP7gzCnJPZ4VRVpCw=\'',
-        ],
-        styleSrc: [self],
-      },
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          connectSrc: [self, googleAnalyticsDomain, 'https://stats.g.doubleclick.net'],
+          defaultSrc: ['\'none\''],
+          fontSrc: [self, 'data:'],
+          imgSrc: [self, googleAnalyticsDomain, googleAnalyticsGtm],
+          objectSrc: [self],
+          scriptSrc: [
+            self,
+            'https://snap.licdn.com',
+            googleAnalyticsDomain,
+            '\'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU=\'',
+            '\'sha256-yJD+OaJ97T/y5QHr3Se9W0AW2baOVjc1FR3Na10Ih+Y=\'',
+            '\'sha256-Zk3m64g3XkpEOa4ilYFv4J6vnDXIEsOJMYonokV/R6E=\'',
+            '\'sha256-zEF/ALwwDYV2nZ+rdYGh2XpjU1lbO3oZ2osZayOlmpw=\'',
+            googleAnalyticsGtm,
+            '\'sha256-xB3p3jeZFuTiApvSQLM9BrzAfwNP7gzCnJPZ4VRVpCw=\'',
+          ],
+          styleSrc: [self],
+        },
+      }
     })
   );
 };
 
-const setReferrerPolicy = (app: Application, policy: string): void => {
-  if (!policy) {
-    throw new Error('Referrer policy configuration is required');
-  }
-
-  app.use(helmet.referrerPolicy({ policy }));
+const setReferrerPolicy = (app: Application): void => {
+  app.use(helmet({
+    referrerPolicy: {
+      policy: 'origin'
+    }
+  })
+  );
 };
 
-const initHelmet = (app: Application, config: HelmetConfig): void => {
+const initHelmet = (app: Application): void => {
   app.use(helmet());
 
   setContentSecurityPolicy(app);
-  setReferrerPolicy(app, config.referrerPolicy);
+  setReferrerPolicy(app);
 };
 
 export { initHelmet };
