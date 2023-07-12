956e2534021d690d4ba5eefc402d263111a0b1ab
ananthakumar-bc <anantha.muthaiya@brickendon.com>
2023-07-12 14:50:01 +0530

    CAS-1318 Search Title link added

--- src/main/assets/scripts/application.js
+++ src/main/assets/scripts/application.js
@@ -1693,7 +1693,7 @@ if (document.querySelector(".oppurtunities_search_click")) {
        $.each(result.search_data.results, function (key, val) {
          mainLothtml +='<li class="app-search-result">';
          mainLothtml +='<h2 class="govuk-heading-s govuk-!-margin-bottom-1">';
-         mainLothtml +='<a class="govuk-link" href="">' + val.projectName + '</a>';
+         mainLothtml +='<a class="govuk-link" href="/digital-outcomes-and-specialists/reviewRecommendation?projectId='+val.projectId+'&lot='+val.lot+'&status='+val.status+'&subStatus=">' + val.projectName + '</a>';
          mainLothtml +='</h2>';
          mainLothtml +='<p class="govuk-body govuk-!-font-size-16 govuk-!-font-weight-bold govuk-!-margin-bottom-1">' + val.buyerName + '';
          mainLothtml +='</p>';
