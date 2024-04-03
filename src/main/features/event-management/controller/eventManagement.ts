f649026cb9321641df6c8dc88b5e1549549af1fb
Sam Thomas <sam.thomas@jumar.co.uk>
2024-04-03 15:25:26 +0100

    CAS-1550 Supplier Website Url - Closed Event

--- src/main/features/event-management/controller/eventManagement.ts
+++ src/main/features/event-management/controller/eventManagement.ts
@@ -1065,9 +1065,9 @@ export const EVENT_MANAGEMENT_CLOSE = async (req: express.Request, res: express.
             supplierDetailsObj.supplierContactEmail =
               supplierFiltedData.contactPoint != undefined ? supplierFiltedData.contactPoint?.email : null;
             supplierDetailsObj.supplierWebsite =
-              supplierFiltedData.identifier != undefined && supplierFiltedData.identifier != null
-                ? supplierFiltedData.identifier?.uri
-                : null;
+                supplierFiltedData.identifier != undefined && supplierFiltedData.identifier != null
+                    ? supplierFiltedData.identifier?.uri?.includes('http') ? supplierFiltedData.identifier.uri : 'https://' + supplierFiltedData.identifier.uri
+                    : null;
             supplierDetailsObj.supplierName =
               supplierFiltedData.name != undefined && supplierFiltedData.name != null ? supplierFiltedData.name : null;
 
